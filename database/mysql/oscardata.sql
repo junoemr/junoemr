d2dc3f740e694fa1772f4b6185d74833279f6cbd
jordan.nicholas <jordan.nicholas@cloudpractice.ca>
2012-10-31 16:47:15 -0700

    Moved lab req 2010 to oscardata from oscardata_on

--- database/mysql/oscardata.sql
+++ database/mysql/oscardata.sql
@@ -394,10 +394,12 @@ INSERT INTO encounterForm VALUES ('Annual V2','../form/formannualV2.jsp?demograp
 INSERT INTO encounterForm VALUES ('ALPHA','../form/formalpha.jsp?demographic_no=','formAlpha', '0');
 INSERT INTO encounterForm VALUES ('Rourke','../form/formrourke.jsp?demographic_no=','formRourke', '0');
 INSERT INTO encounterForm VALUES ('Rourke2006','../form/formrourke2006.jsp?demographic_no=','formRourke2006', '0');
+INSERT INTO encounterForm VALUES ('Rourke2009', '../form/formrourke2009.jsp?demographic_no=', 'formRourke2009', 0 );
 INSERT INTO encounterForm VALUES ('T2Diabetes','../form/formtype2diabete.jsp?demographic_no=','formType2Diabetes', '0');
 INSERT INTO encounterForm VALUES ('Mental Health','../form/formmentalhealth.jsp?demographic_no=','formMentalHealth', '0');
 INSERT INTO encounterForm VALUES ('PeriMenopausal','../form/formperimenopausal.jsp?demographic_no=','formPeriMenopausal', '0');
 INSERT INTO encounterForm VALUES ('Lab Req','../form/formlabreq.jsp?demographic_no=','formLabReq', '0');
+INSERT INTO encounterForm VALUES ('Lab Req 2010','../form/formlabreq10.jsp?demographic_no=','formLabReq10','0');
 INSERT INTO encounterForm VALUES ('MMSE','../form/formmmse.jsp?demographic_no=','formMMSE', '0');
 INSERT INTO encounterForm VALUES ('Pall. Care','../form/formpalliativecare.jsp?demographic_no=','formPalliativeCare', '0');
 INSERT INTO encounterForm VALUES ('AR','../form/formar.jsp?demographic_no=','formAR', '0');
--- database/mysql/oscardata_on.sql
+++ database/mysql/oscardata_on.sql
@@ -13149,7 +13149,8 @@ insert into encounterForm values('AR2005', '../form/formonar.jsp?demographic_no=
 insert into encounterForm values('Ovulation', '../form/formovulation.jsp?demographic_no=', 'formovulation', '0');
 INSERT INTO encounterForm (form_name, form_value, form_table, hidden) VALUES ('Lab Req 2007', '../form/formlabreq07.jsp?demographic_no=', 'formLabReq07', 0);
 INSERT INTO encounterForm (form_name, form_value) VALUES ('Antenatal Lab Req','../form/formlabreq07.jsp?labType=AnteNatal&demographic_no=');
-insert into encounterForm (form_name,form_value,form_table,hidden) Values('Lab Req 2010','../form/formlabreq10.jsp?demographic_no=','formLabReq10',0);
+-- Moved this entry to the main oscar file
+-- insert into encounterForm (form_name,form_value,form_table,hidden) Values('Lab Req 2010','../form/formlabreq10.jsp?demographic_no=','formLabReq10',0);
 
 INSERT INTO billing_payment_type (payment_type) VALUES ('ALTERNATE');
 
