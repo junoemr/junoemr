5183aa222d75c3eff2701259efa1a67dcfca2640
Daine Trinidad <daine@oscarhost.ca>
2012-11-26 11:30:04 -0800

    Commented out rourke 2009 form for BC since it doesn't work too well

--- database/mysql/createdatabase_bc.sh
+++ database/mysql/createdatabase_bc.sh
@@ -6,5 +6,5 @@ USER=$1
 PASSWORD=$2
 DATABASE_NAME=$3
 echo loading rourke2009_from_oscarinit_bc.sql...
-mysql -u${USER} -p$PASSWORD  $DATABASE_NAME  < rourke2009_from_oscarinit_bc.sql
+#mysql -u${USER} -p$PASSWORD  $DATABASE_NAME  < rourke2009_from_oscarinit_bc.sql
 
