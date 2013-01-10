1d95dd2850114196b0b6b1cf1421215202020a16
Daine Trinidad <daine@oscarhost.ca>
2013-01-10 14:03:05 -0800

    Removed uneccessary sql

--- database/mysql/oscarhost_init.sql
+++ database/mysql/oscarhost_init.sql
@@ -3,8 +3,5 @@ update program set defaultServiceRestrictionDays = 0 where defaultServiceRestric
 -- Makes the SQL for EDD Report 2007 faster
 create index formEdited_index ON formBCAR2007(demographic_no, formEdited);
 
--- Takes care of "Prefs" page error
-alter table eform alter patient_independent SET DEFAULT 0;
-
 -- Enable Rich Text Letter
 update eform set status = 1 where form_name = "Rich Text Letter" and fid = 2;
