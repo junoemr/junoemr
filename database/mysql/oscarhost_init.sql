1825ab91b88bea3321238f05a4d6a6935b9ad4a6
Daine Trinidad <daine@oscarhost.ca>
2013-01-10 16:57:35 -0800

    Added an index to make lab uploads faster

--- database/mysql/oscarhost_init.sql
+++ database/mysql/oscarhost_init.sql
@@ -5,3 +5,6 @@ create index formEdited_index ON formBCAR2007(demographic_no, formEdited);
 
 -- Enable Rich Text Letter
 update eform set status = 1 where form_name = "Rich Text Letter" and fid = 2;
+
+-- Makes Lab Uploads faster
+create index measurementVal_index ON measurementsExt(val(5), keyval);
