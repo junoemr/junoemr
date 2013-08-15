3200877b530765dc2e5f42165a1941508b88186f
jordan.nicholas <jordan.nicholas@cloudpractice.ca>
2013-08-15 13:15:34 -0700

    Added an index to the billing_history table

--- database/mysql/oscarhost_init_bc.sql
+++ database/mysql/oscarhost_init_bc.sql
@@ -2,3 +2,6 @@
 
 -- Makes the SQL for EDD Report 2007 faster
 create index formEdited_index ON formBCAR2007(demographic_no, formEdited);
+
+-- Index the billinghistory table in case it gets too big
+create index billing_history_billingmaster_no_index ON billing_history (billingmaster_no);
