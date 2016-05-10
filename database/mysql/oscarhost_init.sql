3dad245508cbb3884f7d289260532191cf415a1f
robert.martin <robert.martin@cloudpractice.ca>
2016-05-10 13:29:18 -0700

    disabled Ritch Text Letter by default for new instances, enabled faxing encounter notes for new instances (OHSUPPORT-3093)

--- database/mysql/oscarhost_init.sql
+++ database/mysql/oscarhost_init.sql
@@ -1,7 +1,7 @@
 update program set defaultServiceRestrictionDays = 0 where defaultServiceRestrictionDays is null;
 
--- Enable Rich Text Letter
-update eform set status = 1 where form_name = "Rich Text Letter" and fid = 2;
+-- Disable Rich Text Letter
+update eform set status = 0 where form_name = "Rich Text Letter" and fid = 2;
 
 -- Makes Lab Uploads faster
 create index measurementVal_index ON measurementsExt(val(5), keyval);
