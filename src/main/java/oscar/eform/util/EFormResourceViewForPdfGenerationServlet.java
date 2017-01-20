bcc78a6fa866580ecc2ac9fb063ad20fcf05d4e7
robert.martin <robert.martin@cloudpractice.ca>
2017-01-20 10:56:52 -0800

    added code to properly serve up local jquery files when generating pdfs

--- src/main/java/oscar/eform/util/EFormResourceViewForPdfGenerationServlet.java
+++ src/main/java/oscar/eform/util/EFormResourceViewForPdfGenerationServlet.java
@@ -9,6 +9,7 @@
 package oscar.eform.util;
 
 import java.io.IOException;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -95,9 +96,15 @@ public final class EFormResourceViewForPdfGenerationServlet extends HttpServlet 
 			url = "/dms/ManageDocument.do?method=display&doc_no" + docNo +
 					"&providerNo="+providerNo;
 		}
-		
-		RequestDispatcher requestDispatcher = request.getRequestDispatcher(url);
-		logger.debug("Request URL:" +url);
-		requestDispatcher.forward(request, response);
+		if(path.equals("share")) { // allow this to serve up share files properly
+			String redirectURI = request.getContextPath() + url;
+			logger.info("Request URI:" + redirectURI);
+			response.sendRedirect(redirectURI);
+		}
+		else {
+			logger.info("Request URL:" +url);
+			RequestDispatcher requestDispatcher = request.getRequestDispatcher(url);
+			requestDispatcher.forward(request, response);
+		}
 	}
 }
