cb7604e4b605a2670be5115793ec4c475b372d6a
robert.martin <robert.martin@cloudpractice.ca>
2017-02-07 09:32:44 -0800

    refactored image rendering servlet to avoid chaining response committed errors when failing to load images

--- src/main/java/org/oscarehr/ui/servlet/ImageRenderingServlet.java
+++ src/main/java/org/oscarehr/ui/servlet/ImageRenderingServlet.java
@@ -77,35 +77,42 @@ public final class ImageRenderingServlet extends HttpServlet {
 
 	@Override
     public final void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		int httpResponseCode = HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
 		try {
 			String source = request.getParameter("source");
+			
 			
 			// for the most part each sub renderer is responsible for everything including
 			// security checks. There's actually not too much point in having a shared
 			// servlet except to save a little bit of work on registering servlets
 			// and a little processing logic.
 			if (Source.local_client.name().equals(source)) {
-				renderLocalClient(request, response);
+				httpResponseCode = renderLocalClient(request, response);
 			} else if (Source.hnr_client.name().equals(source)) {
-				renderHnrClient(request, response);
+				httpResponseCode = renderHnrClient(request, response);
 			} else if (Source.integrator_client.name().equals(source)) {
-				renderIntegratorClient(request, response);
+				httpResponseCode = renderIntegratorClient(request, response);
 			} else if (Source.signature_preview.name().equals(source)) {
-				renderSignaturePreview(request, response);
+				httpResponseCode = renderSignaturePreview(request, response);
 			} else if (Source.signature_stored.name().equals(source)) {
-				renderSignatureStored(request, response);
+				httpResponseCode = renderSignatureStored(request, response);
 			} else if (Source.clinic_logo.name().equals(source)) {
-				renderClinicLogoStored(request, response);
+				httpResponseCode = renderClinicLogoStored(request, response);
 			} else {
 				throw (new IllegalArgumentException("Unknown source type : " + source));
 			}
-		} catch (Exception e) {
+		}
+		catch (Exception e) {
 			if (e.getCause() instanceof SocketException) {
 				logger.warn("An error we can't handle that's expected infrequently. " + e.getMessage());
 			} else {
 				logger.error("Unexpected error. qs=" + request.getQueryString(), e);
-				response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+				httpResponseCode = HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
 			}
+		}
+		if(httpResponseCode != HttpServletResponse.SC_OK) {
+			response.sendError(httpResponseCode);
 		}
 	}
 
@@ -125,94 +132,6 @@ public final class ImageRenderingServlet extends HttpServlet {
 		bos.write(image);
 		bos.flush();
 	}
-
-	private static final void renderIntegratorClient(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		// this expects integratorFacilityId and caisiClientId as a parameter
-
-		LoggedInInfo loggedInInfo=LoggedInInfo.getLoggedInInfoFromSession(request);
-		
-		// security check
-		HttpSession session = request.getSession();
-		Provider provider = (Provider) session.getAttribute(SessionConstants.LOGGED_IN_PROVIDER);
-		if (provider == null) {
-			response.sendError(HttpServletResponse.SC_FORBIDDEN);
-			return;
-		}
-
-		try {
-			// get image
-			Integer integratorFacilityId = Integer.parseInt(request.getParameter("integratorFacilityId"));
-			Integer caisiClientId = Integer.parseInt(request.getParameter("caisiDemographicId"));
-			DemographicWs demographicWs = CaisiIntegratorManager.getDemographicWs(loggedInInfo, loggedInInfo.getCurrentFacility());
-			DemographicTransfer demographicTransfer = demographicWs.getDemographicByFacilityIdAndDemographicId(integratorFacilityId, caisiClientId);
-
-			if (demographicTransfer != null && demographicTransfer.getPhoto() != null) {
-				renderImage(response, demographicTransfer.getPhoto(), "jpeg");
-				return;
-			}
-		} catch (Exception e) {
-			logger.error("Unexpected error.", e);
-		}
-
-		response.sendError(HttpServletResponse.SC_NOT_FOUND);
-	}
-
-	private static final void renderHnrClient(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		// this expects linkingId as a parameter
-
-		LoggedInInfo loggedInInfo=LoggedInInfo.getLoggedInInfoFromSession(request);
-		
-		// security check
-		HttpSession session = request.getSession();
-		Provider provider = (Provider) session.getAttribute(SessionConstants.LOGGED_IN_PROVIDER);
-		if (provider == null) {
-			response.sendError(HttpServletResponse.SC_FORBIDDEN);
-			return;
-		}
-
-		try {
-			// get image
-			Integer linkingId = Integer.parseInt(request.getParameter("linkingId"));
-			org.oscarehr.hnr.ws.Client hnrClient = CaisiIntegratorManager.getHnrClient(loggedInInfo, loggedInInfo.getCurrentFacility(),linkingId);
-
-			if (hnrClient != null && hnrClient.getImage() != null) {
-				renderImage(response, hnrClient.getImage(), "jpeg");
-				return;
-			}
-		} catch (Exception e) {
-			logger.error("Unexpected error.", e);
-		}
-
-		response.sendError(HttpServletResponse.SC_NOT_FOUND);
-	}
-
-	private static final void renderLocalClient(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		// this expects clientId as a parameter
-
-		// security check
-		HttpSession session = request.getSession();
-		Provider provider = (Provider) session.getAttribute(SessionConstants.LOGGED_IN_PROVIDER);
-		if (provider == null) {
-			response.sendError(HttpServletResponse.SC_FORBIDDEN);
-			return;
-		}
-
-		try {
-			// get image
-			org.oscarehr.casemgmt.model.ClientImage clientImage = clientImageDAO.getClientImage(Integer.parseInt(request.getParameter("clientId")));
-			if (clientImage != null && "jpg".equalsIgnoreCase(clientImage.getImage_type())) {
-				renderImage(response, clientImage.getImage_data(), "jpeg");
-				return;
-			} else {
-				renderImage(response, getDefaultImage(request), "jpeg");
-			}
-		} catch (Exception e) {
-			logger.error("Unexpected error.", e);
-		}
-
-		response.sendError(HttpServletResponse.SC_NOT_FOUND);
-	}
-	
 	private static byte[] getDefaultImage(HttpServletRequest request) {
 		String defaultClientImage = "/images/defaultG_img.jpg";
 
@@ -230,15 +149,100 @@ public final class ImageRenderingServlet extends HttpServlet {
 		return bais.toByteArray();
 	}
 
-	private void renderSignaturePreview(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	private static final int renderIntegratorClient(HttpServletRequest request, HttpServletResponse response) {
+		// this expects integratorFacilityId and caisiClientId as a parameter
+
+		LoggedInInfo loggedInInfo=LoggedInInfo.getLoggedInInfoFromSession(request);
+		
+		// security check
+		HttpSession session = request.getSession();
+		Provider provider = (Provider) session.getAttribute(SessionConstants.LOGGED_IN_PROVIDER);
+		if (provider == null) {
+			return HttpServletResponse.SC_FORBIDDEN;
+		}
+
+		try {
+			// get image
+			Integer integratorFacilityId = Integer.parseInt(request.getParameter("integratorFacilityId"));
+			Integer caisiClientId = Integer.parseInt(request.getParameter("caisiDemographicId"));
+			DemographicWs demographicWs = CaisiIntegratorManager.getDemographicWs(loggedInInfo, loggedInInfo.getCurrentFacility());
+			DemographicTransfer demographicTransfer = demographicWs.getDemographicByFacilityIdAndDemographicId(integratorFacilityId, caisiClientId);
+
+			if (demographicTransfer != null && demographicTransfer.getPhoto() != null) {
+				renderImage(response, demographicTransfer.getPhoto(), "jpeg");
+				return HttpServletResponse.SC_OK;
+			}
+		} catch (Exception e) {
+			logger.error("Unexpected error.", e);
+		}
+
+		return HttpServletResponse.SC_NOT_FOUND;
+	}
+
+	private static final int renderHnrClient(HttpServletRequest request, HttpServletResponse response) {
+		// this expects linkingId as a parameter
+
+		LoggedInInfo loggedInInfo=LoggedInInfo.getLoggedInInfoFromSession(request);
+		
+		// security check
+		HttpSession session = request.getSession();
+		Provider provider = (Provider) session.getAttribute(SessionConstants.LOGGED_IN_PROVIDER);
+		if (provider == null) {
+			return HttpServletResponse.SC_FORBIDDEN;
+		}
+
+		try {
+			// get image
+			Integer linkingId = Integer.parseInt(request.getParameter("linkingId"));
+			org.oscarehr.hnr.ws.Client hnrClient = CaisiIntegratorManager.getHnrClient(loggedInInfo, loggedInInfo.getCurrentFacility(),linkingId);
+
+			if (hnrClient != null && hnrClient.getImage() != null) {
+				renderImage(response, hnrClient.getImage(), "jpeg");
+				return HttpServletResponse.SC_OK;
+			}
+		}
+		catch (Exception e) {
+			logger.error("Unexpected error.", e);
+		}
+
+		return HttpServletResponse.SC_NOT_FOUND;
+	}
+
+	private static final int renderLocalClient(HttpServletRequest request, HttpServletResponse response) {
+		// this expects clientId as a parameter
+
+		// security check
+		HttpSession session = request.getSession();
+		Provider provider = (Provider) session.getAttribute(SessionConstants.LOGGED_IN_PROVIDER);
+		if (provider == null) {
+			return HttpServletResponse.SC_FORBIDDEN;
+		}
+
+		try {
+			// get image
+			org.oscarehr.casemgmt.model.ClientImage clientImage = clientImageDAO.getClientImage(Integer.parseInt(request.getParameter("clientId")));
+			if (clientImage != null && "jpg".equalsIgnoreCase(clientImage.getImage_type())) {
+				renderImage(response, clientImage.getImage_data(), "jpeg");
+				return HttpServletResponse.SC_OK;
+			} else {
+				renderImage(response, getDefaultImage(request), "jpeg");
+			}
+		}
+		catch (Exception e) {
+			logger.error("Unexpected error.", e);
+		}
+
+		return HttpServletResponse.SC_NOT_FOUND;
+	}
+	
+	private int renderSignaturePreview(HttpServletRequest request, HttpServletResponse response) {
 		// this expects signatureRequestId as a parameter
 
 		// security check
 		HttpSession session = request.getSession();
 		Provider provider = (Provider) session.getAttribute(SessionConstants.LOGGED_IN_PROVIDER);
 		if (provider == null) {
-			response.sendError(HttpServletResponse.SC_FORBIDDEN);
-			return;
+			return HttpServletResponse.SC_FORBIDDEN;
 		}
 
 		try {
@@ -251,8 +255,9 @@ public final class ImageRenderingServlet extends HttpServlet {
 				byte[] imageBytes = new byte[1024 * 256];
 				fileInputStream.read(imageBytes);
 				renderImage(response, imageBytes, "jpeg");
-				return;
-			} catch (FileNotFoundException e) {
+				return HttpServletResponse.SC_OK;
+			} 
+			catch (FileNotFoundException e) {
 				// no image, render a blank gif, yes this breaks the concept 
 				// of the image already exists, but it's difficult to implement the preview otherwise
 				String tempFilePath = getServletContext().getRealPath("/images/1x1.gif");
@@ -260,7 +265,7 @@ public final class ImageRenderingServlet extends HttpServlet {
 				byte[] imageBytes = new byte[1024 * 32];
 				fileInputStream.read(imageBytes);
 				renderImage(response, imageBytes, "gif");
-				return;
+				return HttpServletResponse.SC_OK;
 			} finally {
 				IOUtils.closeQuietly(fileInputStream);
 			}
@@ -268,18 +273,17 @@ public final class ImageRenderingServlet extends HttpServlet {
 			logger.error("Unexpected error.", e);
 		}
 
-		response.sendError(HttpServletResponse.SC_NOT_FOUND);
+		return HttpServletResponse.SC_NOT_FOUND;
 	}
 	
-	private static final void renderSignatureStored(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	private static final int renderSignatureStored(HttpServletRequest request, HttpServletResponse response) {
 		// this expects digitalSignatureId as a parameter
 
 		// security check
 		HttpSession session = request.getSession();
 		Provider provider = (Provider) session.getAttribute(SessionConstants.LOGGED_IN_PROVIDER);
 		if (provider == null) {
-			response.sendError(HttpServletResponse.SC_FORBIDDEN);
-			return;
+			return HttpServletResponse.SC_FORBIDDEN;
 		}
 
 		try {
@@ -287,23 +291,22 @@ public final class ImageRenderingServlet extends HttpServlet {
 			DigitalSignature digitalSignature = digitalSignatureDao.find(Integer.parseInt(request.getParameter("digitalSignatureId")));
 			if (digitalSignature != null) {
 				renderImage(response, digitalSignature.getSignatureImage(), "jpeg");
-				return;
+				return HttpServletResponse.SC_OK;
 			}
 		} catch (Exception e) {
 			logger.error("Unexpected error.", e);
 		}
 
-		response.sendError(HttpServletResponse.SC_NOT_FOUND);
+		return HttpServletResponse.SC_NOT_FOUND;
 	}
 	
-	private static final void renderClinicLogoStored(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	private static final int renderClinicLogoStored(HttpServletRequest request, HttpServletResponse response) {
 		
 		// security check
 		HttpSession session = request.getSession();
 		Provider provider = (Provider) session.getAttribute(SessionConstants.LOGGED_IN_PROVIDER);
 		if (provider == null) {
-			response.sendError(HttpServletResponse.SC_FORBIDDEN);
-			return;
+			return HttpServletResponse.SC_FORBIDDEN;
 		}
 
 		try {
@@ -315,7 +318,7 @@ public final class ImageRenderingServlet extends HttpServlet {
 					
 					if(data != null) {
 						renderImage(response, data, "jpeg");
-						return;
+						return HttpServletResponse.SC_OK;
 					}
 				}
 			}
@@ -323,6 +326,6 @@ public final class ImageRenderingServlet extends HttpServlet {
 			logger.error("Unexpected error.", e);
 		}
 
-		response.sendError(HttpServletResponse.SC_NOT_FOUND);
+		return HttpServletResponse.SC_NOT_FOUND;
 	}
 }
