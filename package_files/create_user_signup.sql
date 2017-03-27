9d76841fddf86c6d03317bf079db551fd24d180e
Jordan Nicholas <jordan.nicholas@oscarhost.ca>
2017-03-27 11:56:05 -0700

    New user_signup schema and alterted measurements table.

--- package_files/create_user_signup.sql
+++ package_files/create_user_signup.sql
@@ -16,6 +16,29 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `schema_migrations`
+--
+
+DROP TABLE IF EXISTS `schema_migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `schema_migrations` (
+  `version` varchar(255) NOT NULL,
+  UNIQUE KEY `unique_schema_migrations` (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `schema_migrations`
+--
+
+LOCK TABLES `schema_migrations` WRITE;
+/*!40000 ALTER TABLE `schema_migrations` DISABLE KEYS */;
+INSERT INTO `schema_migrations` VALUES ('20170214222606'),('20170214225313'),('20170216011045'),('20170216192618'),('20170217002621'),('20170310184223');
+/*!40000 ALTER TABLE `schema_migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user_signup`
 --
 
@@ -56,6 +79,7 @@ CREATE TABLE `user_signup` (
   `encrypted_oscarhost_password` varchar(200) DEFAULT NULL,
   `current_server_url` text,
   `pin` text,
+  `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`practice_id`),
   UNIQUE KEY `practice_id` (`practice_id`),
   KEY `id_user_signup_status` (`id_user_signup_status`),
@@ -66,13 +90,55 @@ CREATE TABLE `user_signup` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user_signup`
+-- Table structure for table `user_signup_plan`
 --
 
-LOCK TABLES `user_signup` WRITE;
-/*!40000 ALTER TABLE `user_signup` DISABLE KEYS */;
-INSERT INTO `user_signup` VALUES ('Oscar Test Practice','oscar_test','oscar_test','SeemaGanesh7887','-123-35-512331108-126-50-606010948-4520-26980-119-724','Test','User','stephen.mcgrath@oscarhost.ca','1234567890','0','Canada','BC','Victoria','1-415 Dunedin St','','0','Family Medicine','0','0','2012-11-26 07:17:17','96.49.165.233',1,'2012-11-26 07:17:17',0,8,1,'2012-11-26 07:19:09','0',0,0,NULL,NULL,NULL);
-/*!40000 ALTER TABLE `user_signup` ENABLE KEYS */;
+DROP TABLE IF EXISTS `user_signup_plan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_signup_plan` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_signup_plan`
+--
+
+LOCK TABLES `user_signup_plan` WRITE;
+/*!40000 ALTER TABLE `user_signup_plan` DISABLE KEYS */;
+INSERT INTO `user_signup_plan` VALUES (1,'basic','$199/month, up to 6 users',1),(2,'plus','$499/month, up to 15 users',1),(3,'premium','$999/month, up to 40 users',1);
+/*!40000 ALTER TABLE `user_signup_plan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_signup_status`
+--
+
+DROP TABLE IF EXISTS `user_signup_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_signup_status` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` varchar(50) NOT NULL,
+  `description` varchar(200) DEFAULT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_signup_status`
+--
+
+LOCK TABLES `user_signup_status` WRITE;
+/*!40000 ALTER TABLE `user_signup_status` DISABLE KEYS */;
+INSERT INTO `user_signup_status` VALUES (1,'new_user','New unprocessed user',1),(2,'user_db_created','Initial oscar database created',1),(3,'oscar_account_created','Initial oscar user created',1),(4,'case_management_imported','Case Management has been imported',1),(5,'oscar_instance_crawled','Oscar Instance has been crawled and cached',1),(6,'set_account_password','User \'s password has been setup',1),(7,'sent_user_email','User has been sent a welcome email with their login credentials',1),(8,'setup_done','Setup process is done',1),(9,'marked_for_deletion','Marked & ready to be deleted',1);
+/*!40000 ALTER TABLE `user_signup_status` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -84,4 +150,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-03-02 14:22:13
+-- Dump completed on 2017-03-15 12:45:39
