9ee71f748f00684ccc932ab5f8b3872f066f0ff1
Nick Warwick <nick@cloudpractice.ca>
2017-05-31 08:38:14 -0700

    Why is this happening

--- src/main/webapp/web/common/util/util.js
+++ src/main/webapp/web/common/util/util.js
@@ -7,63 +7,53 @@ if (!Juno.Common) Juno.Common = {};
 
 Juno.Common.Util = {};
 
-Juno.Common.Util.exists = function exists(object)
-{
+Juno.Common.Util.exists = function exists(object) {
 	// not undefined and not null
 	return angular.isDefined(object) && object !== null;
 };
 
-Juno.Common.Util.isBlank = function isBlank(object)
-{
+Juno.Common.Util.isBlank = function isBlank(object) {
 	// undefined or null or empty string
 	return !Juno.Common.Util.exists(object) || object === "";
 };
 
-Juno.Common.Util.toArray = function toArray(obj)
-{ //convert single object to array
+Juno.Common.Util.toArray = function toArray(obj) { //convert single object to array
 	if (obj instanceof Array) return obj;
 	else if (obj == null) return [];
 	else return [obj];
 };
 
-Juno.Common.Util.pad0 = function pad0(n)
-{
+Juno.Common.Util.pad0 = function pad0(n) {
 	var s = n.toString();
 	if (s.length == 1) s = "0" + s;
 	return s;
 };
 
-Juno.Common.Util.toTrimmedString = function toTrimmedString(s)
-{
+Juno.Common.Util.toTrimmedString = function toTrimmedString(s) {
 	if (s == null) s = "";
 	if (s instanceof String) s = s.trim();
 	return s;
 };
 
-Juno.Common.Util.formatDate = function formatDate(d)
-{
+Juno.Common.Util.formatDate = function formatDate(d) {
 	d = Juno.Common.Util.toTrimmedString(d);
-	if (d)
-	{
+	if (d) {
 		if (!(d instanceof Date)) d = new Date(d);
 		d = d.getFullYear() + "-" + Juno.Common.Util.pad0(d.getMonth() + 1) + "-" + Juno.Common.Util.pad0(d.getDate());
 	}
 	return d;
 };
 
-Juno.Common.Util.formatTime = function formatTime(d)
-{
+Juno.Common.Util.formatTime = function formatTime(d) {
 	d = Juno.Common.Util.toTrimmedString(d);
-	if (d)
-	{
+	if (d) {
 		if (!(d instanceof Date)) d = new Date(d);
 		d = Juno.Common.Util.pad0(d.getHours()) + ":" + Juno.Common.Util.pad0(d.getMinutes());
 	}
 	return d;
 };
 
-Juno.Common.Util.addNewLine = function addNewLine(line, mssg)
-{
+Juno.Common.Util.addNewLine = function addNewLine(line, mssg) {
 	if (line == null || line.trim() == "") return mssg;
 
 	if (mssg == null || mssg.trim() == "") mssg = line.trim();
