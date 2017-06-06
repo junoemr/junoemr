307f5ce05cee8da20b16c435a126994434b73348
Nick Warwick <nick@cloudpractice.ca>
2017-06-06 15:34:11 -0700

    Create two new controllers; remove previous controller definitions from consultRequstController.js and consultResponsecontroller.js and move them to the new controllers

---
+++ src/main/webapp/web/consults/consultRequestAttachmentController.js
@@ -0,0 +1,83 @@
+angular.module('Consults').controller('Consults.ConsultRequestAttachmentController', [
+
+	'$scope',
+	'$uibModalInstance',
+	'consultService',
+	'consult',
+
+	function(
+		$scope,
+		$uibModalInstance,
+		consultService,
+		consult)
+	{
+
+		var controller = this;
+
+		controller.atth = {};
+		controller.atth.patientName = consult.demographic.lastName + ", " + consult.demographic.firstName;
+		controller.atth.attachedDocs = consult.attachments;
+		if (controller.atth.attachedDocs[0] != null) controller.atth.selectedAttachedDoc = controller.atth.attachedDocs[0];
+
+		var consultId = 0;
+
+		if (consult.id != null) consultId = consult.id;
+		consultService.getRequestAttachments(consultId, consult.demographicId).then(
+			function success(results)
+			{
+				if (consult.availableDocs == null) consult.availableDocs = Juno.Common.Util.toArray(results);
+				controller.atth.availableDocs = consult.availableDocs;
+				Juno.Consults.Common.sortAttachmentDocs(controller.atth.availableDocs);
+				if (controller.atth.availableDocs[0] != null) controller.atth.selectedAvailableDoc = controller.atth.availableDocs[0];
+			},
+			function error(errors)
+			{
+				console.log(errors);
+			});
+
+		controller.openDoc = function openDoc(doc)
+		{
+			window.open("../" + doc.url);
+		};
+
+		controller.attach = function attach()
+		{
+			if (controller.atth.selectedAvailableDoc == null) return;
+
+			controller.atth.attachedDocs.push(controller.atth.selectedAvailableDoc);
+			controller.atth.selectedAttachedDoc = controller.atth.selectedAvailableDoc;
+			controller.atth.selectedAttachedDoc.attached = true;
+			Juno.Consults.Common.sortAttachmentDocs(controller.atth.attachedDocs);
+
+			var selectionIndex = $("#selAvailDoc").val();
+			controller.atth.availableDocs.splice(selectionIndex, 1);
+			if (selectionIndex >= controller.atth.availableDocs.length) selectionIndex = controller.atth.availableDocs.length - 1;
+			controller.atth.selectedAvailableDoc = controller.atth.availableDocs[selectionIndex];
+
+			consult.attachmentsChanged = true;
+		};
+
+		controller.detach = function detach()
+		{
+			if (controller.atth.selectedAttachedDoc == null) return;
+
+			controller.atth.availableDocs.push(controller.atth.selectedAttachedDoc);
+			controller.atth.selectedAvailableDoc = controller.atth.selectedAttachedDoc;
+			controller.atth.selectedAvailableDoc.attached = false;
+			Juno.Consults.Common.sortAttachmentDocs(controller.atth.availableDocs);
+
+			var selectionIndex = $("#selAttachDoc").val();
+			controller.atth.attachedDocs.splice(selectionIndex, 1);
+			if (selectionIndex >= controller.atth.attachedDocs.length) selectionIndex = controller.atth.attachedDocs.length - 1;
+			controller.atth.selectedAttachedDoc = controller.atth.attachedDocs[selectionIndex];
+
+			consult.attachmentsChanged = true;
+		};
+
+		controller.done = function done()
+		{
+			console.log('CLOSE MODAL');
+			$uibModalInstance.close();
+		};
+	}
+]);
--- src/main/webapp/web/consults/consultRequestController.js
+++ src/main/webapp/web/consults/consultRequestController.js
@@ -162,11 +162,11 @@ angular.module('Consults').controller('Consults.ConsultRequestController', [
 
 		controller.changeLetterhead = function changeLetterhead()
 		{
-			var index = $("#letterhead").val();
-			if (index == null) return;
+			var selectionIndex = $("#letterhead").val();
+			if (selectionIndex == null) return;
 
-			consult.letterheadAddress = consult.letterheadList[index].address;
-			consult.letterheadPhone = consult.letterheadList[index].phone;
+			consult.letterheadAddress = consult.letterheadList[selectionIndex].address;
+			consult.letterheadPhone = consult.letterheadList[selectionIndex].phone;
 		};
 
 		controller.changeService = function changeService(id)
@@ -349,8 +349,15 @@ angular.module('Consults').controller('Consults.ConsultRequestController', [
 			var modalInstance = $uibModal.open(
 			{
 				templateUrl: "consults/consultAttachment.jsp",
-				controller: AttachmentCtrl,
-				windowClass: "attachment-modal-window"
+				controller: 'Consults.ConsultRequestAttachmentController as consultAttachmentCtrl',
+				windowClass: "attachment-modal-window",
+				resolve:
+				{
+					consult: function()
+					{
+						return consult;
+					}
+				}
 			});
 
 			modalInstance.result.then(
@@ -367,77 +374,6 @@ angular.module('Consults').controller('Consults.ConsultRequestController', [
 					console.log(errors);
 				});
 		};
-
-		//attachment modal controller
-		function AttachmentCtrl($scope, $uibModalInstance)
-		{
-			controller.atth = {};
-			controller.atth.patientName = consult.demographic.lastName + ", " + consult.demographic.firstName;
-
-			controller.atth.attachedDocs = consult.attachments;
-			if (controller.atth.attachedDocs[0] != null) controller.atth.selectedAttachedDoc = controller.atth.attachedDocs[0];
-
-			var consultId = 0;
-			if (consult.id != null) consultId = consult.id;
-			consultService.getRequestAttachments(consultId, consult.demographicId).then(
-				function success(results)
-				{
-					if (consult.availableDocs == null) consult.availableDocs = Juno.Common.Util.toArray(results);
-					controller.atth.availableDocs = consult.availableDocs;
-					Juno.Common.Util.sortAttachmentDocs(controller.atth.availableDocs);
-					if (controller.atth.availableDocs[0] != null) controller.atth.selectedAvailableDoc = controller.atth.availableDocs[0];
-				},
-				function error(errors)
-				{
-					console.log(errors);
-				});
-
-			controller.openDoc = function openDoc(doc)
-			{
-				window.open("../" + doc.url);
-			};
-
-			controller.attach = function attach()
-			{
-				if (controller.atth.selectedAvailableDoc == null) return;
-
-				controller.atth.attachedDocs.push(controller.atth.selectedAvailableDoc);
-				controller.atth.selectedAttachedDoc = controller.atth.selectedAvailableDoc;
-				controller.atth.selectedAttachedDoc.attached = true;
-				Juno.Consults.Common.sortAttachmentDocs(controller.atth.attachedDocs);
-
-				var x = $("#selAvailDoc").val();
-				controller.atth.availableDocs.splice(x, 1);
-				if (x >= controller.atth.availableDocs.length) x = controller.atth.availableDocs.length - 1;
-				controller.atth.selectedAvailableDoc = controller.atth.availableDocs[x];
-
-				consult.attachmentsChanged = true;
-			};
-
-			controller.detach = function detach()
-			{
-				if (controller.atth.selectedAttachedDoc == null) return;
-
-				controller.atth.availableDocs.push(controller.atth.selectedAttachedDoc);
-				controller.atth.selectedAvailableDoc = controller.atth.selectedAttachedDoc;
-				controller.atth.selectedAvailableDoc.attached = false;
-				Juno.Consults.Common.sortAttachmentDocs(controller.atth.availableDocs);
-
-				var x = $("#selAttachDoc").val();
-				controller.atth.attachedDocs.splice(x, 1);
-				if (x >= controller.atth.attachedDocs.length) x = controller.atth.attachedDocs.length - 1;
-				controller.atth.selectedAttachedDoc = controller.atth.attachedDocs[x];
-
-				consult.attachmentsChanged = true;
-			};
-
-			controller.done = function done()
-			{
-				$uibModalInstance.close();
-			};
-		}
-		//end modal controller
-
 
 		//show/hide e-send button
 		controller.setESendEnabled = function setESendEnabled()
