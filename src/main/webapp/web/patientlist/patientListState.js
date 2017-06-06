097a9438359bfaa898c589fa6d1f27248ee6a188
Nick Warwick <nick@cloudpractice.ca>
2017-06-06 11:16:48 -0700

    Create new service for managing common variables accross controllers in patientlist

---
+++ src/main/webapp/web/patientlist/patientListState.js
@@ -0,0 +1,10 @@
+angular.module("PatientList").service("patientListState", [
+	// Store variables here that must be accessible across controllers in patientlist
+	function()
+	{
+		var service = this;
+
+		service.tabItems = [];
+
+	}
+]);
