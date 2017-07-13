2364a592ddd794dbcb67156bf59a49333f327983
Nick Warwick <nick@cloudpractice.ca>
2017-07-13 13:00:45 -0700

    Use parent class method for setting proprties

--- src/main/java/oscar/form/FrmBCAR2007Record.java
+++ src/main/java/oscar/form/FrmBCAR2007Record.java
@@ -27,7 +27,6 @@ package oscar.form;
 
 import java.sql.SQLException;
 import java.util.Date;
-import java.util.Map;
 import java.util.Properties;
 
 import org.oscarehr.common.dao.DemographicExtDao;
@@ -35,7 +34,6 @@ import org.oscarehr.common.model.Demographic;
 import org.oscarehr.util.LoggedInInfo;
 import org.oscarehr.util.SpringUtils;
 
-import oscar.SxmlMisc;
 import oscar.util.UtilDateUtilities;
 
 public class FrmBCAR2007Record extends FrmRecord {
@@ -48,69 +46,17 @@ public class FrmBCAR2007Record extends FrmRecord {
     	Demographic demo = demographicManager.getDemographic(loggedInInfo, demographicNo);
     	Properties props = new Properties();
 
-        String demographicNoStr = Integer.toString(demographicNo);
-        String formCreated = UtilDateUtilities.DateToString(new Date(),_dateFormat);
-        String surname = demo.getLastName();
-        String firstname = demo.getFirstName();
-        String address = demo.getAddress();
-        String city = demo.getCity();
-        String province = demo.getProvince();
-        String postal = demo.getPostal();
-        String phn = demo.getHin();
-        String dateOfBirth = UtilDateUtilities.DateToString(date, _dateFormat);
-        String age = String.valueOf(UtilDateUtilities.calcAge(date));
-        String phone = demo.getPhone();
-        String phoneAlt = demo.getPhone2();
-        String pg1FormDate = UtilDateUtilities.DateToString(new Date(), _dateFormat);
-        String pg2FormDate = UtilDateUtilities.DateToString(new Date(), _dateFormat);
-        String pg3FormDate = UtilDateUtilities.DateToString(new Date(), _dateFormat);
 
         if (existingID <= 0) {
             if (demo != null) {
                 java.util.Date date = UtilDateUtilities.calcDate(demo.getYearOfBirth(), demo.getMonthOfBirth(), demo.getDateOfBirth());
-
-                if(demographicNoStr != null)
-                    props.setProperty("demographic_no", demographicNoStr);
-                if(formCreated != null)
-                    props.setProperty("formCreated", formCreated);
-                if(surname != null)
-                    props.setProperty("c_surname", surname);
-                if(firstname != null)
-                    props.setProperty("c_givenName", firstname);
-                if(address != null)
-                    props.setProperty("c_address", address);
-                if(city != null)
-                    props.setProperty("c_city", city);
-                if(province != null)
-                    props.setProperty("c_province", province);
-                if(postal != null)
-                    props.setProperty("c_postal", postal);
-                if(phn != null)
-                    props.setProperty("c_phn", postal);
-                if(dateOfBirth != null)
-                    props.setProperty("pg1_dateOfBirth", dateOfBirth);
-                if(age != null)
-                    props.setProperty("pg1_age", age);
-                if(phone != null)
-                    props.setProperty("c_phone", phone);
-                if(phoneAlt != null)
-                    props.setProperty("c_phoneAlt1", phoneAlt);
-                if(pg1FormDate != null)
-                    props.setProperty("pg1_formDate", pg1FormDate);
-                if(pg2FormDate != null)
-                    props.setProperty("pg2_formDate", pg2FormDate);
-                if(pg2FormDate != null)
-                    props.setProperty("pg3_formDate", pg2FormDate);
+                setDemoProperties(loggedInInfo, demographicNo, props);
+               
+                props.setProperty("pg1_formDate", UtilDateUtilities.DateToString(new Date(), _dateFormat));
+                props.setProperty("pg2_formDate", UtilDateUtilities.DateToString(new Date(), _dateFormat));
+                props.setProperty("pg3_formDate", UtilDateUtilities.DateToString(new Date(), _dateFormat));
                 
-                String rd = SxmlMisc.getXmlContent(demo.getFamilyDoctor(), "rd");
-                rd = rd != null ? rd : "";
-                props.setProperty("pg1_famPhy", rd);
-
-                Map<String,String> demoExt = demographicExtDao.getAllValuesForDemo(demographicNo);
-                String cell = demoExt.get("demo_cell");
-                if ( cell != null ){
-                    props.setProperty("c_phoneAlt2",cell );
-                }
+               
             }
         } else {
             String sql = "SELECT * FROM formBCAR2007 WHERE demographic_no = " + demographicNo + " AND ID = " + existingID;
@@ -119,30 +65,8 @@ public class FrmBCAR2007Record extends FrmRecord {
             props = (frh).getFormRecord(sql);
             
             if (demo != null) {
-                if(surname != null)
-                    props.setProperty("c_surname_cur", surname);
-                if(firstname != null)
-                    props.setProperty("c_givenName_cur", firstname);
-                if(address != null)
-                    props.setProperty("c_address_cur", address);
-                if(city != null)
-                    props.setProperty("c_city_cur", city);
-                if(province != null)
-                    props.setProperty("c_province_cur", province);
-                if(postal != null)
-                    props.setProperty("c_postal_cur", postal);
-                if(phn != null)
-                    props.setProperty("c_phn_cur", phn);
-                if(phone != null)
-                    props.setProperty("c_phone_cur", phone);
-                if(phoneAlt != null)
-                    props.setProperty("c_phoneAlt1_cur", phoneAlt);
-                
-                Map<String,String> demoExt = demographicExtDao.getAllValuesForDemo(demographicNo);
-                String cell = demoExt.get("demo_cell");
-                if ( cell != null ){
-                    props.setProperty("c_phoneAlt2_cur",cell );
-                }
+               setDemoCurProperties(loggedInInfo, demographicNo, props);
+               
             }
         }
         return props;
