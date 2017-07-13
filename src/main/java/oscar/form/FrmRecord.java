0db79c9724249a6e1b3cc9115ed9872c5b30e92e
Nick Warwick <nick@cloudpractice.ca>
2017-07-13 13:03:57 -0700

    Set date format so this function actually works

--- src/main/java/oscar/form/FrmRecord.java
+++ src/main/java/oscar/form/FrmRecord.java
@@ -89,6 +89,7 @@ public abstract class FrmRecord {
 		this.setDemographic(loggedInInfo, demographicNo);
 		
         date = UtilDateUtilities.calcDate(demographic.getYearOfBirth(), demographic.getMonthOfBirth(), demographic.getDateOfBirth());
+		dateFormat = "dd/MM/yyyy";
         demoProps.setProperty("demographic_no", demographic.getDemographicNo().toString());
 
         demoProps.setProperty("c_surname", StringUtils.trimToEmpty(demographic.getLastName()));
