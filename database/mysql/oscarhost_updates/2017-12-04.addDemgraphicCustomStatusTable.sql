56a3ee029a21d71233e07c05d8322b21ed9e74b7
Nick Warwick <nick@cloudpractice.ca>
2017-12-05 15:44:13 -0800

    Update database patch to add a deleted column in the demogaphic_custom_status table

--- database/mysql/oscarhost_updates/2017-12-04.addDemgraphicCustomStatusTable.sql
+++ database/mysql/oscarhost_updates/2017-12-04.addDemgraphicCustomStatusTable.sql
@@ -1,3 +1,3 @@
-CREATE TABLE demographic_custom_status (id int(10) AUTO_INCREMENT PRIMARY KEY, status varchar(128));
+CREATE TABLE demographic_custom_status (id int(10) AUTO_INCREMENT PRIMARY KEY, status varchar(128), deleted tinyint(1) NOT NULL DEFAULT 0);
 
 ALTER TABLE demographic ADD COLUMN custom_status_id int(10) AFTER patient_status;
