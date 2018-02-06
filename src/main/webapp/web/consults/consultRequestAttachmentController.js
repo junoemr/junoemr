1f8d7d00be6df8acf34bd42eb8d0f3424c7260c6
Kolby Chapman <kolby.chapman@cloudpractice.ca>
2018-02-06 11:46:23 -0800

    Fix attaching and detaching of documents to consultations in new ui so that the correct one is always attached/detached

--- src/main/webapp/web/consults/consultRequestAttachmentController.js
+++ src/main/webapp/web/consults/consultRequestAttachmentController.js
@@ -45,8 +45,15 @@ angular.module('Consults').controller('Consults.ConsultRequestAttachmentControll
 			controller.atth.selectedAttachedDoc = controller.atth.selectedAvailableDoc;
 			controller.atth.selectedAttachedDoc.attached = true;
 
-			var selectionIndex = $("#selAvailDoc").val();
-			controller.atth.availableDocs.splice(selectionIndex, 1);
+			//Get index to remove by document id
+			var removeIndex = controller.atth.availableDocs.map(function(item)
+			{
+				return item.documentNo;
+			}).indexOf(controller.atth.selectedAvailableDoc.documentNo);
+
+			controller.atth.availableDocs.splice(removeIndex, 1);
+
+			var selectionIndex = $("#selAvailDoc")[0].selectedIndex;
 			if (selectionIndex >= controller.atth.availableDocs.length) selectionIndex = controller.atth.availableDocs.length - 1;
 			controller.atth.selectedAvailableDoc = controller.atth.availableDocs[selectionIndex];
 
@@ -61,8 +68,15 @@ angular.module('Consults').controller('Consults.ConsultRequestAttachmentControll
 			controller.atth.selectedAvailableDoc = controller.atth.selectedAttachedDoc;
 			controller.atth.selectedAvailableDoc.attached = false;
 
-			var selectionIndex = $("#selAttachDoc").val();
-			controller.atth.attachedDocs.splice(selectionIndex, 1);
+			//Get index to remove by document id
+			var removeIndex = controller.atth.attachedDocs.map(function(item)
+			{
+				return item.documentNo;
+			}).indexOf(controller.atth.selectedAttachedDoc.documentNo);
+
+			controller.atth.attachedDocs.splice(removeIndex, 1);
+
+			var selectionIndex = $("#selAttachDoc")[0].selectedIndex
 			if (selectionIndex >= controller.atth.attachedDocs.length) selectionIndex = controller.atth.attachedDocs.length - 1;
 			controller.atth.selectedAttachedDoc = controller.atth.attachedDocs[selectionIndex];
 
