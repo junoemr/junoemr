1024354805f662fd501494c59bdc99c5f3b19627
Velina Ivanova <velina.ivanova@cloudpractice.ca>
2018-04-05 15:39:02 -0700

    Respect clinicaid eligibility response formatting.

--- src/main/java/org/oscarehr/integration/clinicaid/service/ClinicaidAPIService.java
+++ src/main/java/org/oscarehr/integration/clinicaid/service/ClinicaidAPIService.java
@@ -80,7 +80,7 @@ public class ClinicaidAPIService
 		else
 		{
 			PatientEligibilityDataTo1 eligibilityData = result.getData().getEligibilityData();
-			response.put("result", eligibilityData.isEligible() ? "Eligible" : "Not Eligible");
+			response.put("result", eligibilityData.isEligible() ? "Patient Eligible" : "Patient Not Eligible");
 			response.put("msg", eligibilityData.getMessage());
 		}
 
