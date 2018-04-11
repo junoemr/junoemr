27789bc8e0866164641111cb8036464efd3cbb03
robert.martin <robert.martin@cloudpractice.ca>
2018-04-11 11:36:39 -0700

    added max length check on wcb number. changed how the numeric check is done.

--- src/main/java/oscar/oscarBilling/ca/bc/Teleplan/WCBTeleplanSubmission.java
+++ src/main/java/oscar/oscarBilling/ca/bc/Teleplan/WCBTeleplanSubmission.java
@@ -29,6 +29,7 @@ import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.oscarehr.common.dao.DemographicDao;
 import org.oscarehr.common.model.Demographic;
@@ -44,6 +45,9 @@ import oscar.oscarBilling.ca.bc.MSP.TeleplanFileWriter;
  * @author jaygallagher
  */
 public class WCBTeleplanSubmission {
+
+	private static final int WCB_CLAIM_NO_MAX_LENGTH = 8;
+
     private static Logger log = MiscUtils.getLogger();
     
     private DemographicDao demographicDao = null;
@@ -107,13 +111,16 @@ public class WCBTeleplanSubmission {
 	    }
 	    else
 	    {
-		    if(wcb.getW_wcbno() != null && !wcb.getW_wcbno().trim().equals(""))
+		    String wcbNo = wcb.getW_wcbno();
+		    if(wcbNo != null && !wcbNo.trim().isEmpty())
 		    {
-			    try
+			    // check maximum length
+			    if(wcbNo.length() > WCB_CLAIM_NO_MAX_LENGTH)
 			    {
-				    Integer.parseInt(wcb.getW_wcbno());
+				    m.append(": WCB claim # exceeds maximum length of " + WCB_CLAIM_NO_MAX_LENGTH + " digits ");
 			    }
-			    catch(Exception e)
+			    // if not a number
+			    if(!StringUtils.isNumeric(wcbNo))
 			    {
 				    m.append(": WCB claim # may only contain Numbers ");
 			    }
