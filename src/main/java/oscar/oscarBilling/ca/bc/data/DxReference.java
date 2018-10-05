0329700d4f5f296e2b38f387584dbe7e29f8a050
Kolby Chapman <kolby.chapman@cloudpractice.ca>
2018-10-05 13:05:04 -0700

    Use isEmpty()

--- src/main/java/oscar/oscarBilling/ca/bc/data/DxReference.java
+++ src/main/java/oscar/oscarBilling/ca/bc/data/DxReference.java
@@ -110,7 +110,7 @@ public class DxReference {
     private void fillDxCodeDescrition(DxCode code){
 
          List<DiagnosticCode> dxCodeList = diagnosticCodeDao.getByDxCode(code.getDx());
-         if(dxCodeList.size() != 0)
+         if(!dxCodeList.isEmpty())
          {
              DiagnosticCode bdc = dxCodeList.get(0);
              code.setDesc(bdc.getDescription());
