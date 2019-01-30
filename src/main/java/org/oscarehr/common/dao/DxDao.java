a7bf7f8eb2732ee444efdbb968a58d0d1d8fcaa0
Chris Semiao <chris.semiao@cloudpractice.ca>
2019-01-30 10:06:11 -0800

    enumerate dx coding systems and parameterize query strings to protect against sql injection

--- src/main/java/org/oscarehr/common/dao/DxDao.java
+++ src/main/java/org/oscarehr/common/dao/DxDao.java
@@ -24,17 +24,76 @@
 package org.oscarehr.common.dao;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.persistence.Query;
 
+import org.apache.log4j.Logger;
 import org.oscarehr.common.NativeSql;
 import org.oscarehr.common.model.DxAssociation;
 import org.oscarehr.util.MiscUtils;
 import org.springframework.stereotype.Repository;
 
+// To prevent SQL injection attacks, we will enumerate the possible coding systems.  These correspond
+// to structural elements of the SQL query (table names, column names) when looking in the database.
+//
+// Adding a new coding system will require it to be enumerated here.
+enum CodingSystem {
+
+	ICD9 ("icd9"),
+	ICHPPCCODE("ichppccode");
+
+	private String text;
+	private static final Map<String, CodingSystem> MAP;
+	private static Logger logger = MiscUtils.getLogger();
+
+	static
+	{
+		Map<String, CodingSystem> initMap = new ConcurrentHashMap<>();
+		for (CodingSystem system : CodingSystem.values())
+		{
+			initMap.put(system.toString(), system);
+		}
+		MAP = Collections.unmodifiableMap(initMap);
+	}
+
+	CodingSystem(String name)
+	{
+		this.text = name;
+	}
+
+	public String toString()
+	{
+		return this.text;
+	}
+
+	public static CodingSystem fromString(String text)
+	{
+		CodingSystem toReturn = MAP.get(text);
+
+		if (toReturn == null)
+		{
+			String errorMessage = "Invalid coding system specified: " + text;
+			logger.error(errorMessage);
+			throw new IllegalArgumentException(errorMessage);
+		}
+
+		return toReturn;
+	}
+
+	public static String clean(String name)
+	{
+		return CodingSystem.fromString(name).toString();
+	}
+
+}
+
 @Repository
 public class DxDao extends AbstractDao<DxAssociation> {
+	private static Logger logger = MiscUtils.getLogger();
 
 	public DxDao() {
 		super(DxAssociation.class);
@@ -72,6 +131,8 @@ public class DxDao extends AbstractDao<DxAssociation> {
     @SuppressWarnings("unchecked")
 	public List<Object[]> findCodingSystemDescription(String codingSystem, String code) {
 		try {
+			codingSystem = CodingSystem.clean(codingSystem);
+
 			String sql = "SELECT " + codingSystem +", description FROM " + codingSystem + " WHERE " + codingSystem + " = :code";
 			Query query = entityManager.createNativeQuery(sql);
 			query.setParameter("code", code);
@@ -86,43 +147,66 @@ public class DxDao extends AbstractDao<DxAssociation> {
     @SuppressWarnings("unchecked")
 	public List<Object[]> findCodingSystemDescription(String codingSystem, String[] keywords) {
 		try {
-			boolean flag=false;
+			codingSystem = CodingSystem.clean(codingSystem);
 			StringBuilder buf = new StringBuilder("select " + codingSystem + ", description from " + codingSystem);
-			
-			for(String keyword : keywords){
-                if(keyword == null || keyword.trim().equals("")) {
-                	continue;
-                }
-                if(!flag) {
-                	buf.append(" where "); 
-                }
-                if(flag) {
-                	buf.append(" or ");
-                }
-                buf.append(" " + codingSystem + " like '%" + keyword + "%' or description like '%" + keyword + "%' ");
-                flag=true;
-            }
-			
+
+			List<String> keywordsArray = new ArrayList<>();
+
+			for (String keyword : keywords)
+			{
+				if (keyword != null && !keyword.trim().isEmpty())
+				{
+					keywordsArray.add(keyword);
+				}
+			}
+
+			for (int i=0; i<keywordsArray.size(); i++)
+			{
+				if (i == 0)
+				{
+					buf.append(" where ");
+				}
+				else
+				{
+					buf.append(" or ");
+				}
+
+				// We will rely on the persistence library to escape potentially dangerous strings for us by
+				// parameterizing all search strings ('%:Param1%', '%:Param2%', etc)
+				String queryParam = "'%:Param" + i + "%'";
+
+				buf.append(" " + codingSystem + " like " + queryParam + " or description like " + queryParam);
+			}
+
 			Query query = entityManager.createNativeQuery(buf.toString());
+
+			for (int i=0; i<keywordsArray.size(); i++)
+			{
+				String paramName = "Param" + i;
+				query.setParameter(paramName, keywordsArray.get(i));
+			}
 			return query.getResultList();
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			MiscUtils.getLogger().error("error",e);
 			return new ArrayList<Object[]>();
 		}
-		
+
 	}
 	
 	@NativeSql
 	public String getCodeDescription(String codingSystem, String code) {
 		String desc="";
-		StringBuilder buf = new StringBuilder("select description from " + codingSystem + " where "+ codingSystem + "='" + code + "'");
+		codingSystem = CodingSystem.clean(codingSystem);
+		StringBuilder buf = new StringBuilder("select description from " + codingSystem + " where "+ codingSystem + "= :code");
 		try {
 			Query query = entityManager.createNativeQuery(buf.toString());
+			query.setParameter("code", code);
 			desc =  (String) query.getSingleResult();
 		} catch (Exception e) {
 			MiscUtils.getLogger().error("error "+buf,e);
 		}
 		return desc;
 	}
-		
 }
