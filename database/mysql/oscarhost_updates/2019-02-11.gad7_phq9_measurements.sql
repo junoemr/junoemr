2ce5f382b01812c0cdc4f978d0e60fc7d0990753
Levi <levi@cloudpractice.ca>
2019-02-11 15:10:12 -0800

    DB update to add GAD7 and PHQ9 measurements

---
+++ database/mysql/oscarhost_updates/2019-02-11.gad7_phq9_measurements.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO `measurementType` (type, typeDisplayName, typeDescription, measuringInstruction, validation, createDate) VALUES ('PHQ9', 'PHQ9', 'PHQ9', '', 14, '2019-02-11 13:00:00');
+INSERT IGNORE INTO `measurementType` (type, typeDisplayName, typeDescription, measuringInstruction, validation, createDate) VALUES ('GAD7', 'GAD7', 'GAD7', '', 14, '2019-02-11 13:00:00');
