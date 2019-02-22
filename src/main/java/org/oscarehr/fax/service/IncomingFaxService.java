7335480f60d0c3ae5ff22456e1d1df2f6112d87f
robert.martin <robert.martin@cloudpractice.ca>
2019-02-21 16:07:15 -0800

    added name suffix to documents created through inbound fax integration

--- src/main/java/org/oscarehr/fax/service/IncomingFaxService.java
+++ src/main/java/org/oscarehr/fax/service/IncomingFaxService.java
@@ -64,6 +64,7 @@ public class IncomingFaxService
 		document.setDocCreator(ProviderData.SYSTEM_PROVIDER_NO);
 		document.setResponsible(ProviderData.SYSTEM_PROVIDER_NO);
 		document.setDocdesc("");
+		document.setDocfilename("-" + faxAccount.getIntegrationType() + "-" + referenceId + ".pdf");
 
 		document = documentService.uploadNewDemographicDocument(document, documentStream, null);
 		documentService.routeToGeneralInbox(document.getDocumentNo());
