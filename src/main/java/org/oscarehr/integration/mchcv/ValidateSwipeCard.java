0948ea1e05c9bf2f98c018944bb34512ec635bea
bbenetti <ben.benetti@cloudpractice.ca>
2019-02-27 14:38:06 -0800

    change edit demographic card reader form. + form would only accept ON health cards now accepts both BC and ON. + card parsing now handled by HealthCardParser.js

--- src/main/java/org/oscarehr/integration/mchcv/ValidateSwipeCard.java
+++ src/main/java/org/oscarehr/integration/mchcv/ValidateSwipeCard.java
@@ -26,6 +26,23 @@ package org.oscarehr.integration.mchcv;
 public class ValidateSwipeCard extends org.apache.struts.action.ActionForm {
 
     private String magneticStripe;
+    private String lastName;
+    private String firstName;
+    private String hin;
+    private String hinVer;
+    private String sex;
+
+    private String dobYear;
+    private String dobMonth;
+    private String dobDay;
+
+    private String effYear;
+    private String effMonth;
+    private String effDay;
+
+    private String endYear;
+    private String endMonth;
+    private String endDay;
 
     public ValidateSwipeCard() {
         super();
@@ -37,5 +54,160 @@ public class ValidateSwipeCard extends org.apache.struts.action.ActionForm {
 
     public void setMagneticStripe(String magneticStripe) {
         this.magneticStripe = magneticStripe;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getHin()
+    {
+        return hin;
+    }
+
+    public void setHin(String hin)
+    {
+        this.hin = hin;
+    }
+
+    public String getHinVer()
+    {
+        return hinVer;
+    }
+
+    public void setHinVer(String hinVer)
+    {
+        this.hinVer = hinVer;
+    }
+
+    public String getSex()
+    {
+        return sex;
+    }
+
+    public void setSex(String sex)
+    {
+        this.sex = sex;
+    }
+
+    public String getDobYear()
+    {
+        return dobYear;
+    }
+
+    public String getDobYYYYMMdd()
+    {
+        return this.dobYear + this.dobMonth + this.dobDay;
+    }
+
+    public void setDobYear(String dobYear)
+    {
+        this.dobYear = dobYear;
+    }
+
+    public String getDobMonth()
+    {
+        return dobMonth;
+    }
+
+    public void setDobMonth(String dobMonth)
+    {
+        this.dobMonth = dobMonth;
+    }
+
+    public String getDobDay()
+    {
+        return dobDay;
+    }
+
+    public void setDobDay(String dobDay)
+    {
+        this.dobDay = dobDay;
+    }
+
+    public String getEffYYYYMMdd()
+    {
+        return this.getEffYear() + this.getEffMonth() + this.getEffDay();
+    }
+
+    public String getEffYear()
+    {
+        return effYear;
+    }
+
+    public void setEffYear(String effYear)
+    {
+        this.effYear = effYear;
+    }
+
+    public String getEffMonth()
+    {
+        return effMonth;
+    }
+
+    public void setEffMonth(String effMonth)
+    {
+        this.effMonth = effMonth;
+    }
+
+    public String getEffDay()
+    {
+        return effDay;
+    }
+
+    public void setEffDay(String effDay)
+    {
+        this.effDay = effDay;
+    }
+
+    public String getEndYYYYMMdd()
+    {
+        return this.getEndYear() + this.getEndMonth() + this.getEndDay();
+    }
+
+    public String getEndYear()
+    {
+        return endYear;
+    }
+
+    public void setEndYear(String endYear)
+    {
+        this.endYear = endYear;
+    }
+
+    public String getEndMonth()
+    {
+        return endMonth;
+    }
+
+    public void setEndMonth(String endMonth)
+    {
+        this.endMonth = endMonth;
+    }
+
+    public String getEndDay()
+    {
+        return endDay;
+    }
+
+    public void setEndDay(String endDay)
+    {
+        this.endDay = endDay;
     }
 }
