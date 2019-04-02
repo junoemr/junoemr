5a1f2179472e860951bc312911613b2a496864c3
Myan Panikkar <myan@cloudpractice.ca>
2019-04-02 11:24:08 -0700

    Updated SQL patch to reflect column name change (missed name change in 'AFTER' clause)

--- database/mysql/oscarhost_updates/2019-03-18.add_demographic_mother_father_columns.sql
+++ database/mysql/oscarhost_updates/2019-03-18.add_demographic_mother_father_columns.sql
@@ -1,2 +1,2 @@
 ALTER TABLE demographic ADD COLUMN IF NOT EXISTS name_of_mother varchar(32) AFTER `family_doctor_2`;
-ALTER TABLE demographic ADD COLUMN IF NOT EXISTS name_of_father varchar(32) AFTER `nameOfMother`;
+ALTER TABLE demographic ADD COLUMN IF NOT EXISTS name_of_father varchar(32) AFTER `name_of_mother`;
