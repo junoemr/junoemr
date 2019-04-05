f2159ce3c2edd6b5d6045ac4b674b92f3108ae78
bbenetti <ben.benetti@cloudpractice.ca>
2019-04-04 18:45:31 -0700

    modify scheduledate sql as requested.

--- database/mysql/oscarhost_updates/2019-04-04.scheduledate_new_column.sql
+++ database/mysql/oscarhost_updates/2019-04-04.scheduledate_new_column.sql
@@ -1,3 +1,2 @@
-SET sql_safe_updates=0;
-ALTER TABLE scheduledate ADD COLUMN site_id INT(11);
+ALTER TABLE scheduledate ADD COLUMN IF NOT EXISTS site_id INT(11);
 UPDATE scheduledate sd JOIN site st ON sd.reason = st.name SET sd.site_id = st.site_id WHERE sd.site_id IS NULL;
