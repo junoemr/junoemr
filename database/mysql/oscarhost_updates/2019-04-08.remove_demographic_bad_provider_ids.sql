ce628b7d6e00494f6513c308916fe24db6c55645
robert.martin <robert.martin@cloudpractice.ca>
2019-04-08 13:28:03 -0700

    added foreign key constraint to demographic provider_no column

--- database/mysql/oscarhost_updates/2019-04-08.remove_demographic_bad_provider_ids.sql
+++ database/mysql/oscarhost_updates/2019-04-08.remove_demographic_bad_provider_ids.sql
@@ -1,1 +1,3 @@
 UPDATE demographic SET provider_no = NULL WHERE provider_no NOT IN (SELECT provider_no FROM provider);
+
+ALTER TABLE demographic ADD FOREIGN KEY IF NOT EXISTS fk_provider(provider_no) REFERENCES provider(provider_no) ON DELETE SET NULL;
