325b39264005bff614de488437f85f22bf9cd75b
Chris Semiao <chris.semiao@cloudpractice.ca>
2019-04-15 15:38:24 -0700

    update oscarhost_init

--- database/mysql/oscarhost_init.sql
+++ database/mysql/oscarhost_init.sql
@@ -235,3 +235,46 @@ ALTER TABLE consultationRequests ADD COLUMN notification_sent tinyint(1) NOT NUL
 ALTER TABLE billingreferral ADD deleted_at date;
 
 UPDATE appointment_status SET editable=1 WHERE id=2;
+
+DROP PROCEDURE IF EXISTS demographic_referral_source;
+DROP PROCEDURE IF EXISTS demographicArchive_referral_source;
+
+DELIMITER //
+
+CREATE PROCEDURE demographic_referral_source()
+  BEGIN
+  IF NOT EXISTS
+    (SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS
+      WHERE table_name = 'demographic'
+      AND column_name='referral_source_id') THEN
+    ALTER TABLE demographic
+    ADD COLUMN referral_source_id INT DEFAULT NULL,
+    ADD CONSTRAINT demographic_referral_source_id_fk FOREIGN KEY (referral_source_id) REFERENCES referral_source(id) ON DELETE SET NULL;
+  END IF;
+  END //
+
+CREATE PROCEDURE demographicArchive_referral_source()
+  BEGIN
+  IF NOT EXISTS
+    (SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS
+      WHERE table_name = 'demographicArchive'
+      AND column_name='referral_source_id') THEN
+    ALTER TABLE demographicArchive
+    ADD COLUMN referral_source_id INT DEFAULT NULL,
+    ADD CONSTRAINT demographicArchive_referral_source_id_fk FOREIGN KEY (referral_source_id) REFERENCES referral_source(id) ON DELETE SET NULL;
+  END IF;
+  END //
+
+DELIMITER ;
+
+CREATE TABLE IF NOT EXISTS referral_source (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  source VARCHAR(255),
+  updated_at DATETIME DEFAULT NULL,
+  deleted_at DATETIME DEFAULT NULL);
+
+CALL demographic_referral_source();
+CALL demographicArchive_referral_source();
+
+DROP PROCEDURE IF EXISTS demographic_referral_source;
+DROP PROCEDURE IF EXISTS demographicArchive_referral_source;
