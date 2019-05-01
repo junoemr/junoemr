c1a7ee71c2fd11826862b0cb5f3ed22edd12c0f9
bbenetti <ben.benetti@cloudpractice.ca>
2019-04-30 17:42:30 -0700

    EForm Integration tests + fix demographic insert sql as new db update broke it (new columns father / mother name). + centralize sql path strings under SqlFiles.java + write basic eform test to make sure you can navigate to and open an eform without error.

--- src/test/java/integration/tests/EchartTests.java
+++ src/test/java/integration/tests/EchartTests.java
@@ -22,6 +22,7 @@
  */
 package integration.tests;
 
+import integration.tests.sql.SqlFiles;
 import integration.tests.util.SeleniumTestBase;
 import integration.tests.util.junoUtil.Navigation;
 import integration.tests.util.seleniumUtil.PageUtil;
@@ -41,7 +42,6 @@ import java.util.regex.Pattern;
 
 public class EchartTests extends SeleniumTestBase
 {
-	private static final String DEMOGRAPHIC_LOAD_SQL = System.getProperty("basedir") + "/src/test/java/integration/tests/sql/add_demographic.sql";
 	private static final String ECHART_URL = "/oscarEncounter/IncomingEncounter.do?providerNo=" + AuthUtils.TEST_PROVIDER_ID + "&appointmentNo=&demographicNo=1&curProviderNo=&reason=Tel-Progress+Note&encType=&curDate=2019-4-17&appointmentDate=&startTime=&status=";
 
 	@BeforeClass
@@ -52,7 +52,7 @@ public class EchartTests extends SeleniumTestBase
 				"casemgmt_note", "casemgmt_cpp", "casemgmt_issue", "casemgmt_note_ext", "casemgmt_note_link", "casemgmt_note_lock",
 				"casemgmt_tmpsave", "validations", "measurementType", "eChart");
 
-		SchemaUtils.loadFileIntoMySQL(DEMOGRAPHIC_LOAD_SQL);
+		SchemaUtils.loadFileIntoMySQL(SqlFiles.DEMOGRAPHIC_ADD);
 	}
 
 	@Test
@@ -99,7 +99,7 @@ public class EchartTests extends SeleniumTestBase
 		logger.info("Write to encounter note. OK");
 
 		// test auto save
-		Thread.sleep(7000); // oscar auto saves every 5 seconds
+		Thread.sleep(10000); // oscar auto saves every 5 seconds
 		driver.navigate().refresh();
 		newNote = driver.findElement(By.xpath("//textarea[@name='caseNote_note']"));
 		Assert.assertTrue("Auto save note. FAIL", Pattern.compile(myUUID.toString()).matcher(newNote.getText()).find());
---
+++ src/test/java/integration/tests/sql/SqlFiles.java
@@ -0,0 +1,8 @@
+package integration.tests.sql;
+
+public class SqlFiles
+{
+	public static final String SQL_BASE_PATH = System.getProperty("basedir") + "/src/test/java/integration/tests/sql/";
+	public static final String DEMOGRAPHIC_ADD = SQL_BASE_PATH + "add_demographic.sql";
+	public static final String EFORM_ADD_TRAVLE_FORM_V4 = SQL_BASE_PATH + "add_eform_travel_form_v4.sql";
+}
