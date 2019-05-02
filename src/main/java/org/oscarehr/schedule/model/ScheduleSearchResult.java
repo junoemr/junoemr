7a485f2430b7864467fc167f0f99314bd2134e24
Chris Semiao <chris.semiao@cloudpractice.ca>
2019-05-02 16:15:19 -0700

    Implement booking rules into schedule search:  Blackout, Cutoff, PrimaryProvider

---
+++ src/main/java/org/oscarehr/schedule/model/ScheduleSearchResult.java
@@ -0,0 +1,5 @@
+package org.oscarehr.schedule.model;
+
+public class ScheduleSearchResult
+{
+}
---
+++ src/main/java/org/oscarehr/ws/external/soap/v1/transfer/schedule/bookingrules/BookingRuleType.java
@@ -0,0 +1,5 @@
+package org.oscarehr.ws.external.soap.v1.transfer.schedule.bookingrules;
+
+public class BookingRuleType
+{
+}
