9fe95fe618b5134871c85cd38e7a77507390c748
Benjamin Benetti <ben.benetti@cloudpractice.ca>
2019-07-08 14:07:48 -0700

    Dashboard for 'doctor' users + change SQL to not reset permission to 'r' if it already exists, with possibly another value.

--- database/mysql/oscarhost_updates/2019-07-08.doctor_dashboard_permissions.sql
+++ database/mysql/oscarhost_updates/2019-07-08.doctor_dashboard_permissions.sql
@@ -1,3 +1,3 @@
-INSERT INTO secObjPrivilege VALUES ('doctor', '_dashboardDisplay', 'r', 0, 999998) ON DUPLICATE KEY UPDATE privilege = 'r';
-INSERT INTO secObjPrivilege VALUES ('doctor', '_dashboardManager', 'r', 0, 999998) ON DUPLICATE KEY UPDATE privilege = 'r';
-INSERT INTO secObjPrivilege VALUES ('doctor', '_dashboardDrilldown', 'r', 0, 999998) ON DUPLICATE KEY UPDATE privilege = 'r';
+INSERT IGNORE INTO secObjPrivilege VALUES ('doctor', '_dashboardDisplay', 'r', 0, 999998);
+INSERT IGNORE INTO secObjPrivilege VALUES ('doctor', '_dashboardManager', 'r', 0, 999998);
+INSERT IGNORE INTO secObjPrivilege VALUES ('doctor', '_dashboardDrilldown', 'r', 0, 999998);
