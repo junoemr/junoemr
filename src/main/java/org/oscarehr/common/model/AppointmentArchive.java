5a2efc1e419750d25595e3a47e620c52f9013ec9
Myan Panikkar <myan@cloudpractice.ca>
2019-08-13 16:24:51 -0700

    transient tag was not the solution, using an up-to-date BeanUtils.copyProperties was

--- src/main/java/org/oscarehr/common/model/AppointmentArchive.java
+++ src/main/java/org/oscarehr/common/model/AppointmentArchive.java
@@ -42,7 +42,6 @@ import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.persistence.Transient;
 import java.util.Date;
 
 @Entity
@@ -101,7 +100,6 @@ public class AppointmentArchive extends AbstractModel<Integer>  {
 	@Column(name="lastupdateuser")
 	private String lastUpdateUser;
 
-	@Transient
 	@ManyToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="lastupdateuser", referencedColumnName="provider_no", insertable=false, updatable=false)
 	private ProviderData lastUpdateUserRecord;
