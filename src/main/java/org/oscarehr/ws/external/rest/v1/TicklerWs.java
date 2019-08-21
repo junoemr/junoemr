44c9650b0a60804b44297811ac43425b9a84bedf
Benjamin Benetti <ben.benetti@cloudpractice.ca>
2019-08-21 13:02:02 -0700

    Add External Tickler REST Endpoints + endpoint GET tickler/{id} to get a tickler + endpoint PUT tickler/{id} to update a tickler + endpoint POST tickler/ to create a tickler

---
+++ src/main/java/org/oscarehr/ws/external/rest/v1/TicklerWs.java
@@ -0,0 +1,113 @@
+/**
+ * Copyright (c) 2012-2018. CloudPractice Inc. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This software was written for
+ * CloudPractice Inc.
+ * Victoria, British Columbia
+ * Canada
+ */
+
+package org.oscarehr.ws.external.rest.v1;
+
+import io.swagger.v3.oas.annotations.Operation;
+import org.oscarehr.common.dao.TicklerDao;
+import org.oscarehr.common.model.Tickler;
+import org.oscarehr.util.MiscUtils;
+import org.oscarehr.ws.external.rest.AbstractExternalRestWs;
+import org.oscarehr.ws.external.rest.v1.transfer.tickler.TicklerTransferInbound;
+import org.oscarehr.ws.external.rest.v1.transfer.tickler.TicklerTransferOutbound;
+import org.oscarehr.ws.rest.response.RestResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Date;
+
+@Component("TicklerWs")
+@Path("/tickler")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class TicklerWs extends AbstractExternalRestWs
+{
+	@Autowired
+	TicklerDao ticklerDao;
+
+	@GET
+	@Path("/{id}")
+	@Operation(summary = "Retrieve a tickler by id")
+	public RestResponse<TicklerTransferOutbound> getTickler(@PathParam("id") Integer id)
+	{
+		try
+		{
+			Tickler tickler = ticklerDao.find(id);
+			return RestResponse.successResponse(new TicklerTransferOutbound(tickler));
+		}
+		catch (Exception e)
+		{
+			MiscUtils.getLogger().error("Error getting tickler [" + id + "]: " + e.getMessage(), e);
+			throw new RuntimeException("Error getting tickler [" + id + "]: " + e.getMessage(), e);
+		}
+	}
+
+
+	@PUT
+	@Path("/{id}")
+	@Operation(summary = "Update the specified tickler")
+	public RestResponse<TicklerTransferOutbound> updateTickler(@PathParam("id") Integer id, TicklerTransferInbound ticklerIn)
+	{
+		try
+		{
+			Tickler tickler = ticklerDao.find(id);
+			tickler = ticklerIn.copyToTickler(tickler);
+			ticklerDao.merge(tickler);
+			return RestResponse.successResponse(new TicklerTransferOutbound(tickler));
+		}
+		catch (Exception e)
+		{
+			MiscUtils.getLogger().error("Error updating tickler [" + id + "]: " + e.getMessage(), e);
+			throw new RuntimeException("Error updating tickler [" + id + "]: " + e.getMessage(), e);
+		}
+	}
+
+	@POST
+	@Path("/")
+	@Operation(summary="Create a new tickler")
+	public RestResponse<TicklerTransferOutbound> createTickler(TicklerTransferInbound ticklerIn)
+	{
+		try
+		{
+			Tickler tickler = ticklerIn.toTickler();
+			tickler.setUpdateDate(new Date());
+			tickler.setCreator(getOAuthProviderNo());
+
+			ticklerDao.persist(tickler);
+			return RestResponse.successResponse(new TicklerTransferOutbound(tickler));
+		}
+		catch (Exception e)
+		{
+			MiscUtils.getLogger().error("Error creating new tickler: " + e.getMessage(), e);
+			throw new RuntimeException("Error creating new tickler: " + e.getMessage(), e);
+		}
+	}
+}
---
+++ src/main/java/org/oscarehr/ws/external/rest/v1/transfer/tickler/TicklerTransferOutbound.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2012-2018. CloudPractice Inc. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This software was written for
+ * CloudPractice Inc.
+ * Victoria, British Columbia
+ * Canada
+ */
+package org.oscarehr.ws.external.rest.v1.transfer.tickler;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.oscarehr.common.model.Tickler;
+import org.springframework.beans.BeanUtils;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.lang.reflect.InvocationTargetException;
+
+@XmlRootElement
+@Schema(description = "Tickler data transfer object, outbound")
+@JsonIgnoreProperties(ignoreUnknown = true) // Ignore properties that are not defined in this class
+public class TicklerTransferOutbound extends TicklerTransferBase
+{
+	public TicklerTransferOutbound() {}
+
+	public TicklerTransferOutbound(Tickler tickler) throws IllegalAccessException, InvocationTargetException
+	{
+		BeanUtils.copyProperties(tickler, this);
+	}
+}
