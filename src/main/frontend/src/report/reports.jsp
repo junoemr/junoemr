798b35491ed44c854c61622cf1caf24f4369dee9
robert.martin <robert.martin@cloudpractice.ca>
2019-08-22 12:27:25 -0700

    re-order new ui reports (same as old ui) and give them numbers to match old ui

--- src/main/frontend/src/report/reports.jsp
+++ src/main/frontend/src/report/reports.jsp
@@ -56,7 +56,10 @@
 			</form>
 			<div class="list-group">
 				<a  ng-repeat="report in reportsCtrl.getReports() | filter: reportsCtrl.reportFilter "
-				 class="list-group-item default" ng-click="reportsCtrl.selectReport(report)">{{report.name}}</a>
+				 class="list-group-item default" ng-click="reportsCtrl.selectReport(report)">
+					<span>{{report.name}}</span>
+					<span class="badge badge-info numberLabel">{{report.numberLabel}}</span>
+				</a>
 			</div>
 		</div>
 
--- src/main/frontend/src/report/reportsController.js
+++ src/main/frontend/src/report/reportsController.js
@@ -33,90 +33,123 @@ angular.module('Report').controller('Report.ReportsController', [
 
 		controller.reports = [
 			{
-				name: 'Daysheets',
-				templateUrl: 'src/report/report_daysheet.jsp'
+				numberLabel: "1",
+				name: 'EDB List',
+				templateUrl: 'src/report/report_edb_list.jsp'
 			},
 			{
+				numberLabel: "2",
 				name: 'Active Patients',
 				templateUrl: 'src/report/report_iframe.jsp',
 				iframeUrl: '../report/reportactivepatientlist.jsp'
 			},
 			{
-				name: 'Old Patients',
-				templateUrl: 'src/report/report_oldPatients.jsp'
+				numberLabel: "3",
+				name: 'Daysheets',
+				templateUrl: 'src/report/report_daysheet.jsp',
 			},
 			{
-				name: 'Patient Chart List',
-				templateUrl: 'src/report/report_patientChartList.jsp'
-			},
-			{
-				name: 'EDB List',
-				templateUrl: 'src/report/report_edb_list.jsp'
-			},
-			{
+				numberLabel: "5",
 				name: 'Bad Appointments',
 				templateUrl: 'src/report/report_badAppointments.jsp'
 			},
 			{
+				numberLabel: "6",
+				name: 'Patient Chart List',
+				templateUrl: 'src/report/report_patientChartList.jsp'
+			},
+			{
+				numberLabel: "7",
+				name: 'Old Patients',
+				templateUrl: 'src/report/report_oldPatients.jsp'
+			},
+			{
+				numberLabel: "8",
 				name: 'No Show Appointments',
 				templateUrl: 'src/report/report_noShowAppointments.jsp'
 			},
 			{
+				numberLabel: "9",
 				name: 'Consultations',
 				templateUrl: 'src/report/report_iframe.jsp',
 				iframeUrl: '../oscarReport/ConsultationReport.jsp'
 			},
 			{
+				numberLabel: "10",
 				name: 'Lab Requisitions',
 				templateUrl: 'src/report/report_iframe.jsp',
 				iframeUrl: '../oscarReport/LabReqReport.jsp'
 			},
 			{
-				name: 'Preventions',
-				templateUrl: 'src/report/report_iframe.jsp',
-				iframeUrl: '../oscarPrevention/PreventionReporting.jsp'
-			},
-			{
-				name: 'Injections',
-				templateUrl: 'src/report/report_iframe.jsp',
-				iframeUrl: '../oscarReport/InjectionReport2.jsp'
-			},
-			{
+				numberLabel: "11",
 				name: 'Demographic Report Tool',
 				templateUrl: 'src/report/report_iframe.jsp',
 				iframeUrl: '../oscarReport/ReportDemographicReport.jsp'
 			},
 			{
+				numberLabel: "13",
+				name: 'Preventions',
+				templateUrl: 'src/report/report_iframe.jsp',
+				iframeUrl: '../oscarPrevention/PreventionReporting.jsp'
+			},
+			{
+				numberLabel: "14",
 				name: 'Patient Study List',
 				templateUrl: 'src/report/report_iframe.jsp',
 				iframeUrl: '../report/demographicstudyreport.jsp'
 			},
 			{
+				numberLabel: "15",
 				name: 'Chronic Disease Management',
 				templateUrl: 'src/report/report_iframe.jsp',
 				iframeUrl: '../oscarReport/oscarMeasurements/SetupSelectCDMReport.do'
 			},
 			{
+				numberLabel: "16",
 				name: 'Waiting List',
 				templateUrl: 'src/report/report_iframe.jsp',
 				iframeUrl: '../oscarWaitingList/SetupDisplayWaitingList.do?waitingListId='
 			},
 			{
+				numberLabel: "17",
 				name: 'Forms',
 				templateUrl: 'src/report/report_iframe.jsp',
 				iframeUrl: '../report/reportFormRecord.jsp'
 			},
 			{
-				name: 'Clinical',
-				templateUrl: 'src/report/report_iframe.jsp',
-				iframeUrl: '../report/ClinicalReports.jsp'
-			},
-			{
+				numberLabel: "18",
 				name: 'SCBP demographic Report',
 				templateUrl: 'src/report/report_iframe.jsp',
 				iframeUrl: '../report/reportBCARDemo.jsp'
 			},
 			{
+				numberLabel: "19",
+				name: 'Clinical',
+				templateUrl: 'src/report/report_iframe.jsp',
+				iframeUrl: '../report/ClinicalReports.jsp'
+			},
+			{
+				numberLabel: "20",
+				name: 'Injections',
+				templateUrl: 'src/report/report_iframe.jsp',
+				iframeUrl: '../oscarReport/InjectionReport2.jsp'
+			},
+			{
+				numberLabel: "21",
+				name: 'OSIS Report',
+				group: 'Public Health',
+				templateUrl: 'src/report/report_iframe.jsp',
+				iframeUrl: '../oscarReport/OSISReport.jsp'
+			},
+			{
+				numberLabel: "22",
+				name: 'One Time Consult CDS Report',
+				group: 'Public Health',
+				templateUrl: 'src/report/report_iframe.jsp',
+				iframeUrl: '../oscarReport/CDSOneTimeConsultReport.jsp'
+			},
+			{
+				numberLabel: "23",
 				name: 'Report By Template',
 				templateUrl: 'src/report/report_iframe.jsp',
 				iframeUrl: '../oscarReport/reportByTemplate/homePage.jsp'
@@ -153,18 +186,6 @@ angular.module('Report').controller('Report.ReportsController', [
 				name: 'SH Mental Health Report',
 				group: 'CAISI',
 				templateUrl: 'src/report/report_sh_mental_health.jsp'
-			},
-			{
-				name: 'OSIS Report',
-				group: 'Public Health',
-				templateUrl: 'src/report/report_iframe.jsp',
-				iframeUrl: '../oscarReport/OSISReport.jsp'
-			},
-			{
-				name: 'One Time Consult CDS Report',
-				group: 'Public Health',
-				templateUrl: 'src/report/report_iframe.jsp',
-				iframeUrl: '../oscarReport/CDSOneTimeConsultReport.jsp'
 			},
 
 		];
