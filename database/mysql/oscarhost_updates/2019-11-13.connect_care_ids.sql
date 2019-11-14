a7fd146dbcb83ebc9a348d4434d7be1ebdac272e
Benjamin Benetti <ben.benetti@cloudpractice.ca>
2019-11-14 12:09:03 -0800

    Connect Care Id + Add Connect Care lab / department id to sites table / UI.

--- database/mysql/oscarhost_updates/2019-11-13.connect_care_ids.sql
+++ database/mysql/oscarhost_updates/2019-11-13.connect_care_ids.sql
@@ -5,3 +5,7 @@ ALTER TABLE provider ADD COLUMN IF NOT EXISTS alberta_connect_care_id varchar(12
 # add Connect Care ids to clinic record.
 ALTER TABLE clinic ADD COLUMN IF NOT EXISTS alberta_connect_care_lab_id varchar(128);
 ALTER TABLE clinic ADD COLUMN IF NOT EXISTS alberta_connect_care_department_id varchar(128);
+
+# add Connect Care ids to site record.
+ALTER TABLE site ADD COLUMN IF NOT EXISTS alberta_connect_care_lab_id varchar(128);
+ALTER TABLE site ADD COLUMN IF NOT EXISTS alberta_connect_care_department_id varchar(128);
