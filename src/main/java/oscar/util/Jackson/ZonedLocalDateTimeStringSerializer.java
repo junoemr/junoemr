8bf234e2201406340723079b45b1e4568a5a87c5
robert.martin <robert.martin@cloudpractice.ca>
2019-12-13 08:40:51 -0800

    remove unused import line

--- src/main/java/oscar/util/Jackson/ZonedLocalDateTimeStringSerializer.java
+++ src/main/java/oscar/util/Jackson/ZonedLocalDateTimeStringSerializer.java
@@ -25,7 +25,6 @@ package oscar.util.Jackson;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import org.oscarehr.util.MiscUtils;
 
 import java.io.IOException;
 import java.time.ZonedDateTime;
