e2b8f8fbd442805f20489c348246b687036b9504
Benjamin Benetti <ben.benetti@cloudpractice.ca>
2019-12-27 15:08:24 -0800

    Appointment Multisite Dropdown + remove unused import :P

--- src/main/java/org/oscarehr/ws/rest/SitesService.java
+++ src/main/java/org/oscarehr/ws/rest/SitesService.java
@@ -36,7 +36,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import java.util.List;
 
 @Path("/sites")
