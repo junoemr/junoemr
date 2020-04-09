1717857fa64d40154a526ea86830c5f5fc820dc7
Chris Semiao <chris.semiao@cloudpractice.ca>
2020-04-08 17:44:31 -0700

    [OHSUPPORT-7689] BCP Billing Bug Fixes + Account for default non-provider selection (000000) in provider select dropdown

--- src/main/webapp/billing/CA/BC/js/junoBillingHelper.js
+++ src/main/webapp/billing/CA/BC/js/junoBillingHelper.js
@@ -5,23 +5,35 @@ Juno.BillingHelper.BC = Juno.BillingHelper.BC || {};
 
 // Requires jQuery
 
-Juno.BillingHelper.BC._applyBCP = function applyBCP($providerSelect, $facNumInput)
-{
+Juno.BillingHelper.BC._applyBCP = function applyBCP() {
+    if ($providerSelect.val() === "000000") {
+        Juno.BillingHelper.BC._updateFacilityNumber($facNumInput, "");
+        return;
+    }
+    else {
+        Juno.BillingHelper.BC._applyProviderBCP($providerSelect, $facNumInput)
+    }
+};
 
+Juno.BillingHelper.BC._applyProviderBCP = function applyProviderBCP($providerSelect, $facNumInput)
+{
     var providerEndpoint =  Juno.BillingHelper.BC._localJunoInstance
         + "/ws/rs/providerService/provider/" + $providerSelect.val() + "/billing";
 
     jQuery.get(providerEndpoint)
         .done(function (provider)
         {
-            if (provider.bcBCPEligible)
+            if (provider && provider.bcBCPEligible)
             {
                 var clinicEndpoint = Juno.BillingHelper.BC._localJunoInstance + "/ws/rs/clinic/";
 
                 jQuery.get(clinicEndpoint)
                     .done(function(clinic)
                     {
-                        Juno.BillingHelper.BC._updateFacilityNumber($facNumInput, clinic.bcFacilityNumber);
+                        if (clinic && clinic.bcFacilityNumber)
+                        {
+                            Juno.BillingHelper.BC._updateFacilityNumber($facNumInput, clinic.bcFacilityNumber);
+                        }
                     })
                     .fail(Juno.BillingHelper.BC._alertError);
             }
