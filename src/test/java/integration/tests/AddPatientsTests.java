dc7abecb0b7fbed7ba07b7b2b39f59bf43d2e14e
Ailin Zhu <ailin.zhu@cloudpractice.ca>
2020-06-18 13:56:29 -0700

    OHSUPPORT-8053 Add header update code

--- src/test/java/integration/tests/AddPatientsTests.java
+++ src/test/java/integration/tests/AddPatientsTests.java
@@ -1,20 +1,38 @@
+/**
+ * Copyright (c) 2012-2018. CloudPractice Inc. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This software was written for
+ * CloudPractice Inc.
+ * Victoria, British Columbia
+ * Canada
+ */
+
 package integration.tests;
 
 import integration.tests.util.SeleniumTestBase;
-import integration.tests.util.junoUtil.DatabaseUtil;
 import integration.tests.util.junoUtil.Navigation;
 import integration.tests.util.seleniumUtil.PageUtil;
 import junit.framework.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.oscarehr.common.dao.utils.AuthUtils;
-import org.oscarehr.common.dao.utils.SchemaUtils;
-import java.io.IOException;
-import java.sql.SQLException;
 import java.util.Set;
 
 public class AddPatientsTests extends SeleniumTestBase {
@@ -94,17 +112,6 @@ public class AddPatientsTests extends SeleniumTestBase {
 		return PageUtil.isExistsBy(nameRow, driver);
 	}
 
-	@BeforeClass
-	public static void setup() throws SQLException, IllegalAccessException, ClassNotFoundException, InstantiationException, IOException, InterruptedException {
-		SchemaUtils.restoreTable("admission", "demographic",
-				"demographicArchive", "demographiccust", "log", "program", "provider_recent_demographic_access",
-				"casemgmt_note", "casemgmt_cpp", "casemgmt_issue", "casemgmt_note_ext", "casemgmt_note_link", "casemgmt_note_lock",
-				"casemgmt_tmpsave", "validations", "measurementType", "eChart");
-
-		loadSpringBeans();
-		DatabaseUtil.createTestDemographic();
-	}
-
 	@Test
 	public void addPatientsClassicUITest() throws Exception {
 		// login
@@ -113,7 +120,7 @@ public class AddPatientsTests extends SeleniumTestBase {
 		}
 		// open patient search page
 		Thread.sleep(2000);
-		driver.findElement((By.xpath("//a[@title=\"Search for patient records\"]"))).click();
+		driver.findElement(By.xpath("//a[@title=\"Search for patient records\"]")).click();
 		switchToWindow();
 		Thread.sleep(2000);
 
