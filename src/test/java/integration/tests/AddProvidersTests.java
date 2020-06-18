c24ccb1e96054d3a5a20b90b8bfefc8886ae0a3e
Ailin Zhu <ailin.zhu@cloudpractice.ca>
2020-06-18 14:02:52 -0700

    OHSUPPORT-8082 AddProvidersTests is added

--- src/test/java/integration/tests/AddProvidersTests.java
+++ src/test/java/integration/tests/AddProvidersTests.java
@@ -1,72 +1,72 @@
+/**
+ * Copyright (c) 2012-2018. CloudPractice Inc. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This software was written for
+ * CloudPractice Inc.
+ * Victoria, British Columbia
+ * Canada
+ */
 package integration.tests;
 
 import integration.tests.util.SeleniumTestBase;
-import integration.tests.util.junoUtil.DatabaseUtil;
 import integration.tests.util.junoUtil.Navigation;
 import integration.tests.util.seleniumUtil.PageUtil;
 import junit.framework.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.oscarehr.common.dao.utils.AuthUtils;
-import org.oscarehr.common.dao.utils.SchemaUtils;
-
-import java.io.IOException;
-import java.sql.SQLException;
 import java.util.Set;
 
 public class AddProvidersTests extends SeleniumTestBase {
 
-	private static final String[] lastNames = {"Momlastname", "Dadlastname", "Sonlastname"};
-	private static final String[] firstNames = {"Momfirstname", "Dadfirstname", "Sonfirstname"};
-	private static final String[] dobYear = {"1999", "1988", "2008"};
-	private static final String[] dobMonth = {"09", "08", "08"};
-	private static final String[] dobDate = {"09", "08", "08"};
-	private static final String[] sex = {"F", "M", "F"};
-	private static final String[] hin = {"01234567890", "111111111", "9874397159"};
-
-	private static final String language = "English";
-	private static final String title = "MS";
-	private static final String spoken = "English";
+	private static final String[] providerNo = {"100001", "100002", "100003"};
+	private static final String[] lastNames = {"Drlname", "Adminlname", "Nurselname"};
+	private static final String[] firstNames = {"Drfname", "Adminfname", "Nursefname"};
+	private static final String[] type = {"doctor", "admin", "nurse"};
+	private static final String[] specialty = {"Family", "111111111", "9874397159"};
+	private static final String[] team = {"Clinic", "111111111", "9874397159"};
+	private static final String[] sex = {"M", "F", "F"};
+	private static final String[] dob = {"1980-02-02", "1988-08-08", "2000-08-08"};
 	private static final String address = "31 Bastion Square #302";
-	private static final String city = "Victoria";
-	private static final String province = "BC";
-	private static final String postal = "V8W 1J1";
-	private static final String homePhone = "686-8560";
-	private static final String homePhoneExt = "101";
+	private static final String homePhone = "250-686-8560";
 	private static final String workPhone = "+1 888-686-8560";
-	private static final String workPhoneExt = "102";
-	private static final String cellPhone = "+1 888-686-8560";
-	private static final String phoneComment = "Prefer Cell";
-	private static final String newsletter = "No";
-	private static final String aboriginal = "No";
 	private static final String email = "ailin.zhu@cloudpractice.ca";
-	private static final String phrUserName = "TTestLastName";
-	private static final String effYear = "2020";
-	private static final String effMonth = "06";
-	private static final String effDate = "01";
-	private static final String hcType = "BC";
-	private static final String hcRenewYear = "2018";
-	private static final String hcRenewMonth = "08";
-	private static final String hcRenewDate = "08";
-	private static final String countryOfOrigin = "CA";
-	private static final String sin = "987654321";
-	private static final String cytology = "123456789";
-	private static final String motherName = "Mom TestLastName";
-	private static final String fatherName = "Dad TestLastName";
-	private static final String referralDoctor = "Dr Referral";
-	private static final String referralDoctorNo = "111111111";
-	private static final String rosterStatus = "RO";
-	private static final String rosteredYear = "2018";
-	private static final String rosteredMonth = "08";
-	private static final String rosteredDate = "08";
-	private static final String patientStatus = "AC";
-	private static final String chartNo = "10001";
+	private static final String pager = "71077777";
+	private static final String cell = "250-250-2500";
+	private static final String otherPhone = "250-686-8560";
+	private static final String fax = "+1 888-686-8560";
+	private static final String mspNo = "6060666";
+	private static final String thirdPartyBillinNo = "1010101";
+	private static final String billingNo = "1010102";
+	private static final String alternateBillingNo = "1010103";
+	private static final String bcpEligibility = "1";//yes
+	private static final String ihaProviderMnemonic = "PROG17H17-Hydroxyprogesterone";
+	private static final String specialtyCodeNo = "Family010";
+	private static final String groupBillingNo = "CA-123456";
+	private static final String cpsidNo = "987654321";
+	//private static final String billCenter = "123456789";
+	private static final String selfLearningUsername = "druser";
+	private static final String selfLearningPassword = "Welcome@123";
+	private static final String status = "1";//active
 
-	public static void switchToWindow()
+	public static void switchToLastWindow()
 	{
 		Set<String> allHandles = driver.getWindowHandles();
 		allHandles.remove(allHandles.iterator().next());
@@ -95,128 +95,65 @@ public class AddProvidersTests extends SeleniumTestBase {
 		return PageUtil.isExistsBy(nameRow, driver);
 	}
 
-	@BeforeClass
-	public static void setup() throws SQLException, IllegalAccessException, ClassNotFoundException, InstantiationException, IOException, InterruptedException {
-		SchemaUtils.restoreTable("admission", "demographic",
-				"demographicArchive", "demographiccust", "log", "program", "provider_recent_demographic_access",
-				"casemgmt_note", "casemgmt_cpp", "casemgmt_issue", "casemgmt_note_ext", "casemgmt_note_link", "casemgmt_note_lock",
-				"casemgmt_tmpsave", "validations", "measurementType", "eChart");
-
-		loadSpringBeans();
-		DatabaseUtil.createTestDemographic();
-	}
-
 	@Test
-	public void addPatientsClassicUITest() throws Exception {
+	public void addProvidersClassicUITest() throws Exception {
 		// login
 		if (!Navigation.isLoggedIn(driver)) {
 			Navigation.doLogin(AuthUtils.TEST_USER_NAME, AuthUtils.TEST_PASSWORD, AuthUtils.TEST_PIN, Navigation.OSCAR_URL, driver);
 		}
-		// open patient search page
+		// open administration panel
 		Thread.sleep(2000);
-		driver.findElement((By.xpath("//a[@title=\"Search for patient records\"]"))).click();
-		switchToWindow();
-		Thread.sleep(2000);
-
-		// Add a demographic record page
-		driver.findElement(By.xpath(".//a[contains(@href,'demographiccontrol')]")).click();
+		driver.findElement(By.id("admin-panel")).click();
+		switchToLastWindow();
 		Thread.sleep(2000);
 
-		driver.findElement(By.id("last_name")).sendKeys(lastNames[0]);
-		driver.findElement(By.id("first_name")).sendKeys(firstNames[0]);
-		dropdownSelectByValue(By.id("official_lang"), language);
-		dropdownSelectByValue(By.id("title"), title);
-		dropdownSelectByValue(By.xpath("//select[@name='spoken_lang']"), spoken);
-		driver.findElement(By.id("address")).sendKeys(address);
-		driver.findElement(By.id("city")).sendKeys(city);
-		dropdownSelectByValue(By.id("province"), province);
-		driver.findElement(By.id("postal")).sendKeys(postal);
-		driver.findElement(By.id("phone")).sendKeys(homePhone);
-		driver.findElement(By.id("hPhoneExt")).sendKeys(homePhoneExt);
-		driver.findElement(By.xpath("//input[@name='phone2']")).sendKeys(workPhone);
-		driver.findElement(By.xpath("//input[@name='wPhoneExt']")).sendKeys(workPhoneExt);
-		driver.findElement(By.xpath("//input[@name='demo_cell']")).sendKeys(cellPhone);
-		driver.findElement(By.xpath("//textarea[@name='phoneComment']")).sendKeys(phoneComment);
-		dropdownSelectByValue(By.xpath("//select[@name='newsletter']"), newsletter);
-		dropdownSelectByValue(By.xpath("//select[@name='aboriginal']"), aboriginal);
-		driver.findElement(By.id("email")).sendKeys(email);
-		driver.findElement(By.xpath("//input[@name='myOscarUserName']")).sendKeys(phrUserName);
-		driver.findElement(By.id("year_of_birth")).sendKeys(dobYear[0]);
-		driver.findElement(By.id("month_of_birth")).sendKeys(dobMonth[0]);
-		driver.findElement(By.id("date_of_birth")).sendKeys(dobDate[0]);
-		dropdownSelectByValue(By.id("sex"), sex[0]);
-		driver.findElement(By.id("hin")).sendKeys(hin[0]);
-		driver.findElement(By.id("eff_date_year")).sendKeys(effYear);
-		driver.findElement(By.id("eff_date_month")).sendKeys(effMonth);
-		driver.findElement(By.id("eff_date_date")).sendKeys(effDate);
-		driver.findElement(By.id("hc_type")).sendKeys(hcType);
-		driver.findElement(By.id("hc_renew_date_year")).sendKeys(hcRenewYear);
-		driver.findElement(By.id("hc_renew_date_month")).sendKeys(hcRenewMonth);
-		driver.findElement(By.id("hc_renew_date_date")).sendKeys(hcRenewDate);
-		dropdownSelectByValue(By.id("countryOfOrigin"), countryOfOrigin);
-		driver.findElement(By.xpath("//input[@name='sin']")).sendKeys(sin);
-		driver.findElement(By.xpath("//input[@name='cytolNum']")).sendKeys(cytology);
-		driver.findElement(By.id("nameOfMother")).sendKeys(motherName);
-		driver.findElement(By.id("nameOfFather")).sendKeys(fatherName);
-		dropdownSelectByIndex(By.xpath("//select[@name='staff']"), 1);
-		dropdownSelectByIndex(By.xpath("//select[@name='cust1']"), 0);
-		dropdownSelectByIndex(By.xpath("//select[@name='cust4']"), 0);
-		dropdownSelectByIndex(By.xpath("//select[@name='cust2']"), 0);
-		driver.findElement(By.xpath("//input[@name='referral_doctor_name']")).sendKeys(referralDoctor);
-		driver.findElement(By.xpath("//input[@name='referral_doctor_no']")).sendKeys(referralDoctorNo);
-		dropdownSelectByValue(By.id("roster_status"), rosterStatus);
-		driver.findElement(By.xpath("//input[@name='roster_date_year']")).sendKeys(rosteredYear);
-		driver.findElement(By.xpath("//input[@name='roster_date_month']")).sendKeys(rosteredMonth);
-		driver.findElement(By.xpath("//input[@name='roster_date_date']")).sendKeys(rosteredDate);
-		dropdownSelectByValue(By.id("patient_status"), patientStatus);
-		driver.findElement(By.id("chart_no")).sendKeys(chartNo);
-		dropdownSelectByIndex(By.id("name_list_id"), 0);
-		driver.findElement(By.id("waiting_list_note")).sendKeys("Waiting List Note");
-		driver.findElement(By.id("waiting_list_referral_date")).sendKeys("2020-06-06");
-		dropdownSelectByIndex(By.id("rsid"), 0);
-		driver.findElement(By.id("cust3")).sendKeys("Alert Note");
-		driver.findElement(By.id("content")).sendKeys("Notes");
-		driver.findElement(By.id("btnAddRecord")).click();
+		// Add a provider record page
+		driver.findElement(By.xpath(".//a[contains(@rel,'provideraddarecord')]")).click();
 		Thread.sleep(1000);
 
-		Assert.assertNotNull(driver.findElement(By.xpath(".//h2[contains(.,'Successful Addition of a Demographic Record.')]")));
-		Assert.assertTrue(isPatientAdded(lastNames[0], firstNames[0],
-				By.xpath("//a[contains(.,'Back to Demographic Search Page')]"),
-				By.xpath("//input[@class='wideInput']"),
-				By.xpath("//tr[@class='odd']")));
-	}
-
-	@Test
-	public void addPatientsClassicUIQuickFormTest() throws Exception {
-		// login
-		Navigation.doLogin(AuthUtils.TEST_USER_NAME, AuthUtils.TEST_PASSWORD, AuthUtils.TEST_PIN, Navigation.OSCAR_URL, driver);
-		// open patient search page
-		Thread.sleep(2000);
-		driver.findElement((By.xpath("//a[@title=\"Search for patient records\"]"))).click();
-		switchToWindow();
-		Thread.sleep(2000);
-
-		// Add a demographic record page
-		driver.findElement(By.xpath(".//a[contains(@href,'demographicaddrecordcustom')]")).click();
-		Thread.sleep(2000);
-
-		driver.findElement(By.xpath("//input[@name='last_name']")).sendKeys(lastNames[1]);
-		driver.findElement(By.xpath("//input[@name='first_name']")).sendKeys(firstNames[1]);
-		driver.findElement(By.xpath("//input[@name='year_of_birth']")).sendKeys(dobYear[1]);
-		dropdownSelectByValue(By.xpath("//select[@name='month_of_birth']"), dobMonth[1]);
-		dropdownSelectByValue(By.xpath("//select[@name='date_of_birth']"), dobDate[1]);
-		dropdownSelectByValue(By.xpath("//select[@name='sex']"),sex[1]);
-		driver.findElement(By.xpath("//input[@name='hin']")).sendKeys(hin[1]);
-		dropdownSelectByValue(By.xpath("//select[@name='patient_status']"), patientStatus);
-		dropdownSelectByIndex(By.xpath("//select[@name='staff']"), 0);
-		driver.findElement(By.xpath("//input[@name='submit']")).click();
+		driver.findElement(By.xpath("//input[@name='provider_no']")).sendKeys(providerNo[0]);
+		driver.findElement(By.xpath("//input[@value='Suggest']")).click();
+		driver.findElement(By.xpath("//input[@name='last_name']")).sendKeys(lastNames[0]);
+		driver.findElement(By.xpath("//input[@name='first_name']")).sendKeys(firstNames[0]);
+		dropdownSelectByValue(By.id("provider_type"), type[0]);
+		driver.findElement(By.xpath("//input[@name='specialty']")).sendKeys(specialty[0]);
+		driver.findElement(By.xpath("//input[@value='team']")).sendKeys(team[0]);
+		dropdownSelectByValue(By.id("sex"),sex[0]);
+		driver.findElement(By.xpath("//input[@name='dob']")).sendKeys(dob[0]);
+		driver.findElement(By.xpath("//input[@name='address']")).sendKeys(address);
+		driver.findElement(By.xpath("//input[@name='phone']")).sendKeys(homePhone);
+		driver.findElement(By.xpath("//input[@name='workphone']")).sendKeys(workPhone);
+		driver.findElement(By.xpath("//input[@name='email']")).sendKeys(email);
+		driver.findElement(By.xpath("//input[@name='xml_p_pager']")).sendKeys(pager);
+		driver.findElement(By.xpath("//input[@name='xml_p_cell']")).sendKeys(cell);
+		driver.findElement(By.xpath("//input[@name='xml_p_phone2']")).sendKeys(otherPhone);
+		driver.findElement(By.xpath("//input[@name='xml_p_fax']")).sendKeys(fax);
+		driver.findElement(By.xpath("//input[@name='ohip_no']")).sendKeys(mspNo);
+		driver.findElement(By.xpath("//input[@name='rma_no']")).sendKeys(thirdPartyBillinNo);
+		driver.findElement(By.xpath("//input[@name='billing_no']")).sendKeys(billingNo);
+		driver.findElement(By.xpath("//input[@name='hso_no']")).sendKeys(alternateBillingNo);
+		dropdownSelectByValue(By.xpath("//select[@name='bc_bcp_eligible']"), bcpEligibility);
+		driver.findElement(By.xpath("//input[@name='alberta_e_delivery_ids']")).sendKeys(ihaProviderMnemonic);
+		driver.findElement(By.xpath("//input[@name='xml_p_specialty_code']")).sendKeys(specialtyCodeNo);
+		driver.findElement(By.xpath("//input[@name='xml_p_billinggroup_no']")).sendKeys(groupBillingNo);
+		driver.findElement(By.xpath("//input[@name='practitionerNo']")).sendKeys(cpsidNo);
+		dropdownSelectByIndex(By.xpath("//select[@name='billcenter']"), 0);//dropdown empty
+		driver.findElement(By.xpath("//input[@name='xml_p_slpusername']")).sendKeys(selfLearningUsername);
+		driver.findElement(By.xpath("//input[@name='xml_p_slppassword']")).sendKeys(selfLearningPassword);
+		dropdownSelectByValue(By.xpath("//select[@name='status']"), status);
+		driver.findElement(By.xpath("//input[@name='submitbtn']")).click();
 		Thread.sleep(1000);
 
-		Assert.assertNotNull(driver.findElement(By.xpath(".//h2[contains(.,'Successful Addition of a Demographic Record.')]")));
-		Assert.assertTrue(isPatientAdded(lastNames[1], firstNames[1],
+		Assert.assertNotNull(driver.findElement(By.xpath(".//h1[contains(.,'Successful Addition of a Provider Record.')]")));
+		driver.findElement(By.xpath(".//a[contains(.,'User Management')]")).click();
+		driver.findElement(By.xpath(".//a[contains(.,'Search/Edit/Delete Provider Records')]")).click();
+		driver.findElement(By.xpath("//input[@name='keyword']")).sendKeys(lastNames[0]);
+		driver.findElement(By.xpath("//input[@name='button']")).click();
+		PageUtil.isExistsBy(By.xpath("//tr[@bgcolor='white']"), driver);
+/*		Assert.assertTrue(isPatientAdded(lastNames[0], firstNames[0],
 				By.xpath("//a[contains(.,'Back to Demographic Search Page')]"),
 				By.xpath("//input[@class='wideInput']"),
-				By.xpath("//tr[@class='odd']")));
+				By.xpath("//tr[@class='odd']")));*/
 	}
 
 	@Test
