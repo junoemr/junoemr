e062b3b8fbab40e9b14a90c0ab70e6fa348aaf70
Ailin Zhu <ailin.zhu@cloudpractice.ca>
2020-06-19 15:00:23 -0700

    OHSUPPORT-8082 AddProvidersTests Classic Part

--- src/test/java/integration/tests/AddProvidersTests.java
+++ src/test/java/integration/tests/AddProvidersTests.java
@@ -28,15 +28,14 @@ import integration.tests.util.seleniumUtil.PageUtil;
 import junit.framework.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.support.ui.Select;
 import org.oscarehr.common.dao.utils.AuthUtils;
 import java.util.Set;
 
 public class AddProvidersTests extends SeleniumTestBase {
 
-	private static final String[] providerNo = {"100001", "100002", "100003"};
+	private static final String[] providerNo = {"200001", "200002", "200003"};
 	private static final String[] lastNames = {"Drlname", "Adminlname", "Nurselname"};
 	private static final String[] firstNames = {"Drfname", "Adminfname", "Nursefname"};
 	private static final String[] type = {"doctor", "admin", "nurse"};
@@ -61,7 +60,6 @@ public class AddProvidersTests extends SeleniumTestBase {
 	private static final String specialtyCodeNo = "Family010";
 	private static final String groupBillingNo = "CA-123456";
 	private static final String cpsidNo = "987654321";
-	//private static final String billCenter = "123456789";
 	private static final String selfLearningUsername = "druser";
 	private static final String selfLearningPassword = "Welcome@123";
 	private static final String status = "1";//active
@@ -86,15 +84,6 @@ public class AddProvidersTests extends SeleniumTestBase {
 		doctor.selectByIndex(dropdownIndex);
 	}
 
-	public static boolean isPatientAdded(String lastName, String firstName, By searchPage, By searchTerm, By nameRow) throws InterruptedException {
-		driver.findElement(searchPage).click();
-		WebElement searchTermField = driver.findElement(searchTerm);
-		searchTermField.sendKeys(lastName + ", " + firstName);
-		searchTermField.sendKeys(Keys.ENTER);
-		Thread.sleep(1000);
-		return PageUtil.isExistsBy(nameRow, driver);
-	}
-
 	@Test
 	public void addProvidersClassicUITest() throws Exception {
 		// login
@@ -108,16 +97,18 @@ public class AddProvidersTests extends SeleniumTestBase {
 		Thread.sleep(2000);
 
 		// Add a provider record page
-		driver.findElement(By.xpath(".//a[contains(@rel,'provideraddarecord')]")).click();
-		Thread.sleep(1000);
-
-		driver.findElement(By.xpath("//input[@name='provider_no']")).sendKeys(providerNo[0]);
+		driver.findElement(By.xpath(".//h5[contains(.,'Add a Provider Record')]")).click();
+		driver.switchTo().frame("myFrame");
 		driver.findElement(By.xpath("//input[@value='Suggest']")).click();
+		Assert.assertNotNull(driver.findElement(By.xpath("//input[@name='provider_no']")));
+
+		driver.findElement(By.xpath("//input[@name='provider_no']")).clear();
+		driver.findElement(By.xpath("//input[@name='provider_no']")).sendKeys(providerNo[0]);
 		driver.findElement(By.xpath("//input[@name='last_name']")).sendKeys(lastNames[0]);
 		driver.findElement(By.xpath("//input[@name='first_name']")).sendKeys(firstNames[0]);
 		dropdownSelectByValue(By.id("provider_type"), type[0]);
 		driver.findElement(By.xpath("//input[@name='specialty']")).sendKeys(specialty[0]);
-		driver.findElement(By.xpath("//input[@value='team']")).sendKeys(team[0]);
+		driver.findElement(By.xpath("//input[@name='team']")).sendKeys(team[0]);
 		dropdownSelectByValue(By.id("sex"),sex[0]);
 		driver.findElement(By.xpath("//input[@name='dob']")).sendKeys(dob[0]);
 		driver.findElement(By.xpath("//input[@name='address']")).sendKeys(address);
@@ -126,13 +117,16 @@ public class AddProvidersTests extends SeleniumTestBase {
 		driver.findElement(By.xpath("//input[@name='email']")).sendKeys(email);
 		driver.findElement(By.xpath("//input[@name='xml_p_pager']")).sendKeys(pager);
 		driver.findElement(By.xpath("//input[@name='xml_p_cell']")).sendKeys(cell);
+
+		//Scroll the web page till end.
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,document.body.scrollHeight)");
 		driver.findElement(By.xpath("//input[@name='xml_p_phone2']")).sendKeys(otherPhone);
 		driver.findElement(By.xpath("//input[@name='xml_p_fax']")).sendKeys(fax);
 		driver.findElement(By.xpath("//input[@name='ohip_no']")).sendKeys(mspNo);
 		driver.findElement(By.xpath("//input[@name='rma_no']")).sendKeys(thirdPartyBillinNo);
 		driver.findElement(By.xpath("//input[@name='billing_no']")).sendKeys(billingNo);
 		driver.findElement(By.xpath("//input[@name='hso_no']")).sendKeys(alternateBillingNo);
-		dropdownSelectByValue(By.xpath("//select[@name='bc_bcp_eligible']"), bcpEligibility);
 		driver.findElement(By.xpath("//input[@name='alberta_e_delivery_ids']")).sendKeys(ihaProviderMnemonic);
 		driver.findElement(By.xpath("//input[@name='xml_p_specialty_code']")).sendKeys(specialtyCodeNo);
 		driver.findElement(By.xpath("//input[@name='xml_p_billinggroup_no']")).sendKeys(groupBillingNo);
@@ -145,18 +139,19 @@ public class AddProvidersTests extends SeleniumTestBase {
 		Thread.sleep(1000);
 
 		Assert.assertNotNull(driver.findElement(By.xpath(".//h1[contains(.,'Successful Addition of a Provider Record.')]")));
+		driver.switchTo().defaultContent();
 		driver.findElement(By.xpath(".//a[contains(.,'User Management')]")).click();
 		driver.findElement(By.xpath(".//a[contains(.,'Search/Edit/Delete Provider Records')]")).click();
-		driver.findElement(By.xpath("//input[@name='keyword']")).sendKeys(lastNames[0]);
+		Thread.sleep(1000);
+		driver.switchTo().frame("myFrame");
+		driver.findElement(By.xpath("//input[@value='search_providerno']")).click();
+		driver.findElement(By.xpath("//input[@name='keyword']")).sendKeys(providerNo[0]);
 		driver.findElement(By.xpath("//input[@name='button']")).click();
-		PageUtil.isExistsBy(By.xpath("//tr[@bgcolor='white']"), driver);
-/*		Assert.assertTrue(isPatientAdded(lastNames[0], firstNames[0],
-				By.xpath("//a[contains(.,'Back to Demographic Search Page')]"),
-				By.xpath("//input[@class='wideInput']"),
-				By.xpath("//tr[@class='odd']")));*/
+		Assert.assertTrue(PageUtil.isExistsBy(By.xpath("//tr[contains(.,providerNo[0])]"), driver));
+
 	}
 
-	@Test
+	/*@Test
 	public void addPatientsJUNOUITest() throws Exception {
 		// login
 		Navigation.doLogin(AuthUtils.TEST_USER_NAME, AuthUtils.TEST_PASSWORD, AuthUtils.TEST_PIN, Navigation.OSCAR_URL, driver);
@@ -189,6 +184,6 @@ public class AddProvidersTests extends SeleniumTestBase {
 				By.xpath("//button[@title='Search']"),
 				By.xpath("//input[@placeholder='Search Term']"),
 				By.xpath("//tr[@class='ng-scope']")));
-	}
+	}*/
 }
 
