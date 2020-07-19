752422a96065f0fc9c7963495387f4c6c722164e
HanZhou <han.zhou@cloudpractice.ca>
2020-07-19 14:28:57 -0700

    style: remove test log3

--- src/test/java/org/oscarehr/common/dao/CtlDocTypeDaoTest.java
+++ src/test/java/org/oscarehr/common/dao/CtlDocTypeDaoTest.java
@@ -25,7 +25,6 @@ package org.oscarehr.common.dao;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
 
 import java.util.List;
 
