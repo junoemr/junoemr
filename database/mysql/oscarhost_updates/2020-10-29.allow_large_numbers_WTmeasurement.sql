b01eceac42d7f084917d68ecd09db46252ce8e60
Eleanor Morris <eleanor.morris@cloudpractice.ca>
2020-11-06 15:46:57 -0800

    removed subquery which accomodated safe-mode

--- database/mysql/oscarhost_updates/2020-10-29.allow_large_numbers_WTmeasurement.sql
+++ database/mysql/oscarhost_updates/2020-10-29.allow_large_numbers_WTmeasurement.sql
@@ -3,7 +3,7 @@ UPDATE validations
   isDate = NULL,
   minLength = NULL,
   maxLength = NULL
-WHERE id = (SELECT id from validations WHERE name = 'Numeric Value greater than or equal to 0');
+WHERE name = 'Numeric Value greater than or equal to 0';
 
 UPDATE measurementType
   SET validation = (SELECT id FROM validations WHERE name = 'Numeric Value greater than or equal to 0')
