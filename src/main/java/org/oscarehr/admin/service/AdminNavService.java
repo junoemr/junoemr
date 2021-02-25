cb25453a1035888410408ae5123f326b85c406b9
Chris Semiao <chris.semiao@cloudpractice.ca>
2021-02-25 11:10:46 -0800

    Start new sysadmin page for transitioning properties to db

--- src/main/java/org/oscarehr/admin/service/AdminNavService.java
+++ src/main/java/org/oscarehr/admin/service/AdminNavService.java
@@ -555,6 +555,11 @@ public class AdminNavService
 			}
 		}
 
+		if (securityInfoManager.isSuperAdmin(providerNo))
+		{
+			systemManagementItems.add( new AdminNavItemTo1 ("System Properties", "admin.systemProperties"));
+		}
+
 		systemManagementGroup.setItems(systemManagementItems);
 		return systemManagementGroup;
 	}
