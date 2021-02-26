6c07ef1dad997df56ef7495bcf723269bea1dc77
Myan Panikkar <myan@cloudpractice.ca>
2021-02-26 11:28:03 -0800

    Update init patch to ignore system provider and providers who already have an entry.
    Also add in a bit to handle *any* providers that have the property set
    but have it default set to "no".

--- database/mysql/oscarhost_init/2021-02-26.default_login_to_juno_ui.sql
+++ database/mysql/oscarhost_init/2021-02-26.default_login_to_juno_ui.sql
@@ -1,6 +1,12 @@
-INSERT INTO `property`(`name`, `value`, provider_no)
+INSERT IGNORE INTO `property`(`name`, `value`, provider_no)
 SELECT
     "cobalt",
     "yes",
     provider_no
-FROM provider;
+FROM provider
+WHERE provider_no != "-1";
+
+-- provider_no="999998" does get a default value, but is set to "no" prior to this
+UPDATE `property`
+SET `value`="yes"
+WHERE `name`="cobalt";
