cdc50b53d9ec5e903824c0cf36217f1e0e812e2e
Eleanor Morris <eleanor.morris@cloudpractice.ca>
2021-03-01 23:19:53 -0800

    use moment.js

--- src/main/frontend/src/patient/addDemographicModal/addDemographicModalComponent.js
+++ src/main/frontend/src/patient/addDemographicModal/addDemographicModalComponent.js
@@ -41,7 +41,7 @@ angular.module('Patient').component('addDemographicModal', {
 		ctrl.newDemographicData.hin = "";
 		ctrl.newDemographicData.ver = "";
 		ctrl.newDemographicData.hcType = "BC";
-		ctrl.newDemographicData.dateJoined = new Date();
+		ctrl.newDemographicData.dateJoined = new moment();
 
 		// address data
 		ctrl.newDemographicData.address = {
