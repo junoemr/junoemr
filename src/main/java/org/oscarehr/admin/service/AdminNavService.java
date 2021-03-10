09f7c5679c15b38c04d7572ebb4f30ae7ac62547
Benjamin Benetti <ben.benetti@cloudpractice.ca>
2021-03-09 16:53:17 -0800

    remove comment that doesn't make sense any more

--- src/main/java/org/oscarehr/admin/service/AdminNavService.java
+++ src/main/java/org/oscarehr/admin/service/AdminNavService.java
@@ -424,7 +424,6 @@ public class AdminNavService
 		scheduleItems.add(new AdminNavItemTo1(resourceBundle.getString("admin.admin.btnGroupPreference"), "frame?frameUrl=" + contextPath + "/admin/groupPreferences.jsp"));
 		scheduleItems.add(new AdminNavItemTo1(resourceBundle.getString("admin.admin.preventionNotification.title"), "frame?frameUrl=" + contextPath + "/oscarPrevention/PreventionManager.jsp"));
 
-		// TODO remove super admin requirement when ready for general use
 		if(systemPreferenceService.isPreferenceEnabled(UserProperty.AQS_INTEGRATION_ENABLED, false))
 		{
 			scheduleItems.add(new AdminNavItemTo1("Manage Appointment Queues", "manageAppointmentQueues"));
