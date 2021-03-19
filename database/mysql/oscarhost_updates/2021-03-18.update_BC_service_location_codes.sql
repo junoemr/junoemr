b0ce832e9325aa0d069cec0714964063be0dbf73
Eleanor Morris <eleanor.morris@cloudpractice.ca>
2021-03-19 16:32:39 -0700

    create primary key and use INSERT IGNORE

--- database/mysql/oscarhost_updates/2021-03-18.update_BC_service_location_codes.sql
+++ database/mysql/oscarhost_updates/2021-03-18.update_BC_service_location_codes.sql
@@ -1,53 +1,11 @@
-INSERT INTO billingvisit (`visittype`, `visit_desc`, `region`)
-SELECT * FROM (SELECT "B", "Community Health Centre", "BC") AS tmp
-WHERE NOT EXISTS (
-    SELECT visittype FROM billingvisit WHERE visittype = "B"
-) LIMIT 1;
+ALTER TABLE `billingvisit` ADD PRIMARY KEY IF NOT EXISTS (`visittype`);
+INSERT IGNORE INTO billingvisit (`visittype`, `visit_desc`, `region`) VALUES ("B", "Community Health Centre", "BC");
+INSERT IGNORE INTO billingvisit (`visittype`, `visit_desc`, `region`) VALUES ("J", "First Nations Primary Health Care Clinic", "BC");
+INSERT IGNORE INTO billingvisit (`visittype`, `visit_desc`, `region`) VALUES ("K", "Hybrid Primary Care Practice (part-time longitudinal practice, part-time walk-in clinic)", "BC");
+INSERT IGNORE INTO billingvisit (`visittype`, `visit_desc`, `region`) VALUES ("L", "Hybrid Primary Care Practice (part-time longitudinal practice, part-time walk-in clinic)", "BC");
+INSERT IGNORE INTO billingvisit (`visittype`, `visit_desc`, `region`) VALUES ("N", "Health Care Practitioner Office (non-physician)", "BC");
+INSERT IGNORE INTO billingvisit (`visittype`, `visit_desc`, `region`) VALUES ("Q", "Specialist Physician Office", "BC");
+INSERT IGNORE INTO billingvisit (`visittype`, `visit_desc`, `region`) VALUES ("U", "Urgent and Primary Care Centre", "BC");
+INSERT IGNORE INTO billingvisit (`visittype`, `visit_desc`, `region`) VALUES ("V", "Virtual Care Clinic", "BC");
+INSERT IGNORE INTO billingvisit (`visittype`, `visit_desc`, `region`) VALUES ("W", "Walk-In Clinic", "BC");
 
-INSERT INTO billingvisit (`visittype`, `visit_desc`, `region`)
-SELECT * FROM (SELECT "J", "First Nations Primary Health Care Clinic", "BC") AS tmp
-WHERE NOT EXISTS (
-    SELECT visittype FROM billingvisit WHERE visittype = "J"
-) LIMIT 1;
-
-INSERT INTO billingvisit (`visittype`, `visit_desc`, `region`)
-SELECT * FROM (SELECT "K", "Hybrid Primary Care Practice (part-time longitudinal practice, part-time walk-in clinic)", "BC") AS tmp
-WHERE NOT EXISTS (
-    SELECT visittype FROM billingvisit WHERE visittype = "K"
-) LIMIT 1;
-
-INSERT INTO billingvisit (`visittype`, `visit_desc`, `region`)
-SELECT * FROM (SELECT "L", "Hybrid Primary Care Practice (part-time longitudinal practice, part-time walk-in clinic)", "BC") AS tmp
-WHERE NOT EXISTS (
-    SELECT visittype FROM billingvisit WHERE visittype = "L"
-) LIMIT 1;
-
-INSERT INTO billingvisit (`visittype`, `visit_desc`, `region`)
-SELECT * FROM (SELECT "N", "Health Care Practitioner Office (non-physician)", "BC") AS tmp
-WHERE NOT EXISTS (
-    SELECT visittype FROM billingvisit WHERE visittype = "N"
-) LIMIT 1;
-
-INSERT INTO billingvisit (`visittype`, `visit_desc`, `region`)
-SELECT * FROM (SELECT "Q", "Specialist Physician Office", "BC") AS tmp
-WHERE NOT EXISTS (
-    SELECT visittype FROM billingvisit WHERE visittype = "Q"
-) LIMIT 1;
-
-INSERT INTO billingvisit (`visittype`, `visit_desc`, `region`)
-SELECT * FROM (SELECT "U", "Urgent and Primary Care Centre", "BC") AS tmp
-WHERE NOT EXISTS (
-    SELECT visittype FROM billingvisit WHERE visittype = "U"
-) LIMIT 1;
-
-INSERT INTO billingvisit (`visittype`, `visit_desc`, `region`)
-SELECT * FROM (SELECT "V", "Virtual Care Clinic", "BC") AS tmp
-WHERE NOT EXISTS (
-    SELECT visittype FROM billingvisit WHERE visittype = "V"
-) LIMIT 1;
-
-INSERT INTO billingvisit (`visittype`, `visit_desc`, `region`)
-SELECT * FROM (SELECT "W", "Walk-In Clinic", "BC") AS tmp
-WHERE NOT EXISTS (
-    SELECT visittype FROM billingvisit WHERE visittype = "W"
-) LIMIT 1;
