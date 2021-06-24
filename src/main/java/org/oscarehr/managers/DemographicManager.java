d95848e70202a00a8cbaabbedd8422e30d7d17fa
Chris <chris.semiao@cloudpractice.ca>
2021-06-24 11:10:22 -0700

    Reapply null check to patient status date

--- src/main/java/org/oscarehr/managers/DemographicManager.java
+++ src/main/java/org/oscarehr/managers/DemographicManager.java
@@ -370,12 +370,12 @@ public class DemographicManager {
 		Date previousStatusDate = previousDemographic.getPatientStatusDate();
 		String currentStatus = demographic.getPatientStatus();
 		Date currentStatusDate = demographic.getPatientStatusDate();
-
+		
 		if (previousStatusDate == null || !currentStatus.equals(previousStatus))
 		{
 			demographic.setPatientStatusDate(new Date());
 		}
-		else if (previousStatusDate.compareTo(currentStatusDate) != 0)
+		else if (!previousStatusDate.equals(currentStatusDate))
 		{
 			demographic.setPatientStatusDate(currentStatusDate);
 		}
