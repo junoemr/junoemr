c708f04aabb03c0db3ef34c3b1986850637aea21
Benjamin Benetti <ben.benetti@cloudpractice.ca>
2021-06-30 10:05:26 -0700

    add other sex types to sex enum

--- src/main/frontend/src/lib/demographic/model/Sex.ts
+++ src/main/frontend/src/lib/demographic/model/Sex.ts
@@ -1,6 +1,9 @@
 export enum Sex {
 	Male = "M",
 	Female = "F",
+	Transgender = "T",
+	Other = "O",
+	Undefined = "U",
 }
 
 export function sexToHuman(sex: Sex): string
@@ -11,5 +14,11 @@ export function sexToHuman(sex: Sex): string
 			return "Male";
 		case Sex.Female:
 			return "Female";
+		case Sex.Transgender:
+			return "Transgender";
+		case Sex.Other:
+			return "Other";
+		case Sex.Undefined:
+			return "Undefined";
 	}
 }
