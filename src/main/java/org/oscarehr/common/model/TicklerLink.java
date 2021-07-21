226cf7e044f16285295e13ea160ce6f8fde48321
Benjamin Benetti <ben.benetti@cloudpractice.ca>
2021-07-21 12:26:37 -0700

    remove unused import

--- src/main/java/org/oscarehr/common/model/TicklerLink.java
+++ src/main/java/org/oscarehr/common/model/TicklerLink.java
@@ -35,7 +35,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
