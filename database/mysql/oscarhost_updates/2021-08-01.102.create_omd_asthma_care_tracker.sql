8233714ba95781832941463b469d524bc3fd4b91
robert.martin <robert.martin@cloudpractice.ca>
2021-08-04 14:38:00 -0700

    re-do the omd asthma care_tracker patch using procedures

--- database/mysql/oscarhost_updates/2021-08-01.102.create_omd_asthma_care_tracker.sql
+++ database/mysql/oscarhost_updates/2021-08-01.102.create_omd_asthma_care_tracker.sql
@@ -1,215 +1,59 @@
+
+SET @care_tracker_name = "Asthma";
+SET @creatorId = "-1"; -- system provider id
+
+SET @rule_name_never_entered = "Warn: Never Entered";
+SET @rule_name_6m_plus = "Warn: Over 6 months since last entry";
+# SET @rule_name_12m_plus = "Warn: Over 12 months since last entry";
+# SET @rule_name_3m_6m = "Note: 3-6 months since last entry";
+# SET @rule_name_10m_12m = "Note: 10-12 months since last entry";
+SET @rule_name_value_over_0 = "Warn: Number Greater Than 0";
+# SET @rule_name_value_over_2 = "Warn: Number Greater Than 2";
+SET @rule_name_value_over_4 = "Warn: Number Greater Than 4";
+SET @rule_name_problem_indicator_checked = "Problem indicator checked";
+
 START TRANSACTION;
 
-INSERT INTO `care_tracker` (care_tracker_name, description, system_managed, enabled, created_at, created_by, updated_at, updated_by)
-SELECT
-    "Asthma" AS care_tracker_name,
-    "Measurements for tracking Asthma",
-    1,
-    1,
-    NOW(),
-    "-1",
-    NOW(),
-    "-1"
-WHERE "Asthma" NOT IN (
-    SELECT care_tracker_name
-    FROM care_tracker
-    WHERE care_tracker_name = "Asthma"
-    AND system_managed IS TRUE
-);
+CALL addCareTracker(@care_tracker_name, "Measurements for tracking Asthma", TRUE);
 
-INSERT INTO care_tracker_item_group(care_tracker_id, group_name, description, created_at, created_by, updated_at, updated_by)
-SELECT
-    id,
-    "Asthma Measurements",
-    "Measurement group for Asthma related measurements",
-    NOW(),
-    "-1",
-    NOW(),
-    "-1"
-FROM care_tracker f
-WHERE f.care_tracker_name="Asthma"
-AND f.system_managed IS TRUE
-AND f.id NOT IN (SELECT care_tracker_id FROM care_tracker_item_group);
+-- set up icd9 triggers
+CALL addCareTrackerIcd9Trigger(@care_tracker_name, "493");
 
--- *** BEGIN INSERT care_tracker_item entries ***
+-- set up the drools connection
+CALL addCareTrackerDrools(@care_tracker_name, "diab.drl", "diabetes decision support");
 
-INSERT INTO `care_tracker_item`(care_tracker_id, care_tracker_item_group_id, item_name, item_type, item_type_code, value_type, value_label, graphable, guideline, description, created_at, created_by, updated_at, updated_by)
-SELECT
-	sheet.id,
-	item_group.id,
-	type.typeDisplayName,
-	"MEASUREMENT",
-	type.type,
-	"STRING",
-	NULL,
-	0,
-	"Number >= 0" AS guideline,
-	type.typeDescription AS description,
-	NOW(),
-	"-1",
-	NOW(),
-	"-1"
-FROM care_tracker sheet
-JOIN care_tracker_item_group item_group ON sheet.id = item_group.care_tracker_id
-JOIN validations v ON v.name="Numeric Value greater than or equal to 0"
-JOIN measurementType type ON type.validation = v.id
-WHERE type.type IN ("ANR", "FEV1", "SPIR", "ANSY", "ASYM", "PEFR")
-AND item_group.group_name = "Asthma Measurements"
-AND type.type NOT IN (
-	SELECT item_type_code
-	FROM care_tracker_item
-	WHERE care_tracker_id=sheet.id AND care_tracker_item_group_id=item_group.id
-);
+-- *** measurements group ***
+SET @group_name = "Asthma Measurements";
+CALL addCareTrackerItemGroup(@care_tracker_name, @group_name, "Measurement group for Asthma related measurements");
 
-INSERT INTO `care_tracker_item`(care_tracker_id, care_tracker_item_group_id, item_name, item_type, item_type_code, value_type, value_label, graphable, guideline, description, created_at, created_by, updated_at, updated_by)
-SELECT
-	sheet.id,
-	item_group.id,
-	type.typeDisplayName,
-	"MEASUREMENT",
-	type.type,
-	"BOOLEAN",
-	NULL,
-	0,
-	"Yes/No" AS guideline,
-	type.typeDescription AS description,
-	NOW(),
-	"-1",
-	NOW(),
-	"-1"
-FROM care_tracker sheet
-JOIN care_tracker_item_group item_group ON sheet.id = item_group.care_tracker_id
-JOIN validations v ON v.name="Yes/No/NA"
-JOIN measurementType type ON type.validation = v.id
-WHERE type.type IN ("ALPA", "ASWA", "AELV", "ARAD", "ARMA", "ARDT", "SMCS", "ASTA", "AENC", "ACOS", "AACP", "AEDR", "ASPR", "LHAD", "OUTR")
-AND item_group.group_name = "Asthma Measurements"
-AND type.type NOT IN (
-	SELECT item_type_code
-	FROM care_tracker_item
-	WHERE care_tracker_id=sheet.id AND care_tracker_item_group_id=item_group.id
-);
-
--- *** END INSERT care_tracker_item entries ***
-
-INSERT INTO drools (drl_file, description, created_at, updated_at)
-SELECT
-    'diab.drl',
-    'diabetes decision support',
-    NOW(),
-    NOW()
-    WHERE 'diab.drl' NOT IN
-(
-    SELECT drl_file
-    FROM drools
-    WHERE drl_file = 'diab.drl'
-    AND description = 'diabetes decision support'
-);
-
-INSERT INTO care_tracker_drools
-SELECT
-    drl.id,
-    sheet.id
-FROM care_tracker sheet
-JOIN drools drl ON drl.drl_file = 'diab.drl'
-WHERE sheet.care_tracker_name = "Asthma" AND system_managed IS TRUE
-AND sheet.id NOT IN (
-    SELECT care_tracker_id
-    FROM care_tracker_drools
-    WHERE sheet.care_tracker_name = "Asthma" AND system_managed IS TRUE
-);
-
-
-INSERT INTO care_tracker_item_ds_rule(care_tracker_item_id, ds_rule_id)
-SELECT
-    items.id,
-    rules.id
-FROM care_tracker_item items
-JOIN care_tracker sheet ON items.care_tracker_id = sheet.id
-JOIN ds_rule rules ON rules.rule_name = "Problem indicator checked" AND rules.system_managed IS TRUE
-WHERE sheet.care_tracker_name = "Asthma"
-AND sheet.system_managed IS TRUE
-AND items.item_type_code IN ("ALPA", "ASWA", "AELV")
-AND items.id NOT IN (
-    SELECT care_tracker_item_id
-    FROM care_tracker_item_ds_rule
-);
-
-INSERT INTO care_tracker_item_ds_rule(care_tracker_item_id, ds_rule_id)
-SELECT
-    items.id,
-    rules.id
-FROM care_tracker_item items
-JOIN care_tracker sheet ON items.care_tracker_id = sheet.id
-JOIN ds_rule rules ON rules.rule_name = "Warn: Number Greater Than 4" AND rules.system_managed IS TRUE
-WHERE sheet.care_tracker_name = "Asthma"
-AND sheet.system_managed IS TRUE
-AND items.item_type_code IN ("ANR", "ASYM")
-AND items.id NOT IN (
-    SELECT care_tracker_item_id
-    FROM care_tracker_item_ds_rule
-);
-
-INSERT INTO care_tracker_item_ds_rule(care_tracker_item_id, ds_rule_id)
-SELECT
-    items.id,
-    rules.id
-FROM care_tracker_item items
-JOIN care_tracker sheet ON items.care_tracker_id = sheet.id
-JOIN ds_rule rules ON rules.rule_name = "Warn: Number Greater Than 0" AND rules.system_managed IS TRUE
-WHERE sheet.care_tracker_name = "Asthma"
-AND sheet.system_managed IS TRUE
-AND items.item_type_code IN ("ANSY")
-AND items.id NOT IN (
-    SELECT care_tracker_item_id
-    FROM care_tracker_item_ds_rule
-);
-
-INSERT INTO care_tracker_item_ds_rule(care_tracker_item_id, ds_rule_id)
-SELECT
-    items.id,
-    rules.id
-FROM care_tracker_item items
-JOIN care_tracker sheet ON items.care_tracker_id = sheet.id
-JOIN ds_rule rules ON rules.rule_name = "Warn: Never Entered" AND rules.system_managed IS TRUE
-WHERE sheet.care_tracker_name = "Asthma"
-AND sheet.system_managed IS TRUE
-AND items.item_type_code IN ("SPIR")
-AND items.id NOT IN (
-    SELECT care_tracker_item_id
-    FROM care_tracker_item_ds_rule
-    JOIN ds_rule rules ON rules.rule_name = "Warn: Never Entered" AND rules.system_managed IS TRUE
-);
-
-INSERT INTO care_tracker_item_ds_rule(care_tracker_item_id, ds_rule_id)
-SELECT
-    items.id,
-    rules.id
-FROM care_tracker_item items
-JOIN care_tracker sheet ON items.care_tracker_id = sheet.id
-JOIN ds_rule rules ON rules.rule_name = "Warn: Over 6 months since last entry" AND rules.system_managed IS TRUE
-WHERE sheet.care_tracker_name = "Asthma"
-AND sheet.system_managed IS TRUE
-AND items.item_type_code IN ("SPIR")
-AND items.id NOT IN (
-    SELECT care_tracker_item_id
-    FROM care_tracker_item_ds_rule
-    JOIN ds_rule rules_inner ON rules_inner.rule_name = "Warn: Over 6 months since last entry" AND rules_inner.system_managed IS TRUE
-    WHERE rules_inner.id != rules.id
-);
-
-INSERT INTO care_tracker_triggers_icd9(care_tracker_id, icd9_id)
-SELECT
-    f.id,
-    i.id
-FROM care_tracker f
-JOIN icd9 i ON i.icd9 = "493"
-WHERE f.care_tracker_name="Asthma"
-AND f.system_managed IS TRUE
-AND f.id NOT IN (
-    SELECT care_tracker_id
-    FROM care_tracker_triggers_icd9
-	JOIN icd9 icd_inner ON icd_inner.icd9 = "493"
-    WHERE icd9_id = icd_inner.id
-);
+CALL addCareTrackerItem(@care_tracker_name, @group_name, "MEASUREMENT", "ALPA", "BOOLEAN", "Asthma Limits Physical Activity", 0, "Yes/No");
+CALL addCareTrackerItemRule(@care_tracker_name, "ALPA", @rule_name_problem_indicator_checked);
+CALL addCareTrackerItem(@care_tracker_name, @group_name, "MEASUREMENT", "ANR", "NUMERIC", "Frequency/Week", 1, "Number >= 0");
+CALL addCareTrackerItemRule(@care_tracker_name, "ANR", @rule_name_value_over_4);
+CALL addCareTrackerItem(@care_tracker_name, @group_name, "MEASUREMENT", "ASYM", "NUMERIC", "Frequency/Week", 1, "dyspnea, cough, wheeze, chest tightness");
+CALL addCareTrackerItemRule(@care_tracker_name, "ASYM", @rule_name_value_over_4);
+CALL addCareTrackerItem(@care_tracker_name, @group_name, "MEASUREMENT", "ASWA", "BOOLEAN", "Has Absence", 0, "School/Work");
+CALL addCareTrackerItemRule(@care_tracker_name, "ASWA", @rule_name_problem_indicator_checked);
+CALL addCareTrackerItem(@care_tracker_name, @group_name, "MEASUREMENT", "ANSY", "NUMERIC", "Frequency/Week", 1, "frequency per week");
+CALL addCareTrackerItemRule(@care_tracker_name, "ANSY", @rule_name_value_over_0);
+CALL addCareTrackerItem(@care_tracker_name, @group_name, "MEASUREMENT", "FEV1", "BOOLEAN", "90% personal or predicted best", 0, "greater than 90 percent personal or predicted or personal best");
+CALL addCareTrackerItem(@care_tracker_name, @group_name, "MEASUREMENT", "AELV", "BOOLEAN", "Has", 0, "Requiring clinical evaluations");
+CALL addCareTrackerItemRule(@care_tracker_name, "AELV", @rule_name_problem_indicator_checked);
+CALL addCareTrackerItem(@care_tracker_name, @group_name, "MEASUREMENT", "SPIR", "NUMERIC", "Spirometry", 1, "Number >= 0");
+CALL addCareTrackerItemRule(@care_tracker_name, "SPIR", @rule_name_never_entered);
+CALL addCareTrackerItemRule(@care_tracker_name, "SPIR", @rule_name_6m_plus);
+CALL addCareTrackerItem(@care_tracker_name, @group_name, "MEASUREMENT", "PEFR", "NUMERIC", "PEFR value", 1, "best of 3");
+CALL addCareTrackerItem(@care_tracker_name, @group_name, "MEASUREMENT", "ARAD", "BOOLEAN", "Reviewed", 0, "Review Asthma Definition");
+CALL addCareTrackerItem(@care_tracker_name, @group_name, "MEASUREMENT", "ARMA", "BOOLEAN", "Reviewed", 0, "Asthma Review Med Adherence");
+CALL addCareTrackerItem(@care_tracker_name, @group_name, "MEASUREMENT", "ARDT", "BOOLEAN", "Reviewed", 0, NULL);
+CALL addCareTrackerItem(@care_tracker_name, @group_name, "MEASUREMENT", "SMCS", "BOOLEAN", "Reviewed", 0, NULL);
+CALL addCareTrackerItem(@care_tracker_name, @group_name, "MEASUREMENT", "ASTA", "BOOLEAN", "Reviewed", 0, NULL);
+CALL addCareTrackerItem(@care_tracker_name, @group_name, "MEASUREMENT", "AENC", "BOOLEAN", "Reviewed", 0, NULL);
+CALL addCareTrackerItem(@care_tracker_name, @group_name, "MEASUREMENT", "ACOS", "BOOLEAN", "Reviewed", 0, NULL);
+CALL addCareTrackerItem(@care_tracker_name, @group_name, "MEASUREMENT", "AACP", "BOOLEAN", "Provided/Revised/Reviewed", 0, NULL);
+CALL addCareTrackerItem(@care_tracker_name, @group_name, "MEASUREMENT", "AEDR", "BOOLEAN", "Referred", 0, NULL);
+CALL addCareTrackerItem(@care_tracker_name, @group_name, "MEASUREMENT", "ASPR", "BOOLEAN", "Referred", 0, NULL);
+CALL addCareTrackerItem(@care_tracker_name, @group_name, "MEASUREMENT", "LHAD", "BOOLEAN", "Admission", 0, NULL);
+CALL addCareTrackerItem(@care_tracker_name, @group_name, "MEASUREMENT", "OUTR", "BOOLEAN", "Referred", 0, NULL);
 
 COMMIT;
