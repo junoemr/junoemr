9bdf4a306578804e737cd5352ee1599e8e5fef0e
robert.martin <robert.martin@cloudpractice.ca>
2021-08-17 10:51:23 -0700

    also change demographic table to utf8 charset to prevent foreign key issues

--- database/mysql/oscarhost_updates/2021-07-20.change_provider_characters_utf8.sql
+++ database/mysql/oscarhost_updates/2021-07-20.change_provider_characters_utf8.sql
@@ -1,4 +1,7 @@
 
+SET FOREIGN_KEY_CHECKS=0;
+ALTER TABLE demographic CONVERT TO CHARACTER SET utf8;
+
 ALTER TABLE provider CONVERT TO CHARACTER SET utf8;
 ALTER TABLE provider MODIFY COLUMN comments mediumtext;
 ALTER TABLE provider MODIFY COLUMN alberta_e_delivery_ids mediumtext;
