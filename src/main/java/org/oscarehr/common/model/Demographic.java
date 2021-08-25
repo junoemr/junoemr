4747766889f06d9b80333f61e12e8bb0f5dce4ec
robert.martin <robert.martin@cloudpractice.ca>
2021-08-25 12:24:47 -0700

    fix null string in family doctor xml tags preventing junoUI save. removed family doctor tags saved to referral doctor field when in classic ui

--- src/main/java/org/oscarehr/common/model/Demographic.java
+++ src/main/java/org/oscarehr/common/model/Demographic.java
@@ -624,6 +624,10 @@ public class Demographic implements Serializable
 	 */
 	public String getFamilyDoctor2()
 	{
+		if(familyDoctor2 != null)
+		{
+			return familyDoctor2.replaceAll(">null<", "><");
+		}
 		return familyDoctor2;
 	}
 
