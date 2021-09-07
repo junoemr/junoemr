ea1d2675d11e7434125576395f1b9a2f889bbd12
Evan Vassiliadis <evan.vassiliadis@cloudpractice.ca>
2021-09-07 15:54:47 -0700

    fixed casemgmt_note compression to be not null

--- database/mysql/oscarhost_updates_applied/2021-09-07.compress_colums.sql
+++ database/mysql/oscarhost_updates_applied/2021-09-07.compress_colums.sql
@@ -2,6 +2,6 @@ ALTER TABLE log_ws_soap MODIFY soap_input TEXT COMPRESSED NULL;
 ALTER TABLE log_ws_soap MODIFY soap_output TEXT COMPRESSED NULL;
 ALTER TABLE log_ws_rest MODIFY raw_output TEXT COMPRESSED NULL;
 ALTER TABLE log_ws_rest MODIFY raw_post TEXT COMPRESSED NULL;
-ALTER TABLE casemgmt_note MODIFY history mediumtext COMPRESSED NULL;
+ALTER TABLE casemgmt_note MODIFY history mediumtext COMPRESSED NOT NULL;
 ALTER TABLE eform_data MODIFY form_data mediumtext COMPRESSED NULL;
 ALTER TABLE eChart MODIFY encounter text COMPRESSED NULL;
