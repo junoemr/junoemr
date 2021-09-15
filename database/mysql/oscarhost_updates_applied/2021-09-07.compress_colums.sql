3ee1c7bea92b9c1ebb1cc107f17c78deb0e1f1b9
Evan Vassiliadis <evan.vassiliadis@cloudpractice.ca>
2021-09-15 10:24:50 -0700

    Increase the innodb buffer pool size before and set it back after

--- database/mysql/oscarhost_updates_applied/2021-09-07.compress_colums.sql
+++ database/mysql/oscarhost_updates_applied/2021-09-07.compress_colums.sql
@@ -1,3 +1,4 @@
+SET GLOBAL innodb_buffer_pool_size = 268435456;
 ALTER TABLE log_ws_soap MODIFY soap_input TEXT COMPRESSED NULL;
 ALTER TABLE log_ws_soap MODIFY soap_output TEXT COMPRESSED NULL;
 ALTER TABLE log_ws_rest MODIFY raw_output TEXT COMPRESSED NULL;
@@ -5,3 +6,4 @@ ALTER TABLE log_ws_rest MODIFY raw_post TEXT COMPRESSED NULL;
 ALTER TABLE casemgmt_note MODIFY history mediumtext COMPRESSED NOT NULL;
 ALTER TABLE eform_data MODIFY form_data mediumtext COMPRESSED NULL;
 ALTER TABLE eChart MODIFY encounter text COMPRESSED NULL;
+SET GLOBAL innodb_buffer_pool_size = 134217728;
