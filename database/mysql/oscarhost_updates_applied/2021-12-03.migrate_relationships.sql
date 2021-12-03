e20cc644fcb8c4d5d50162c4e95ea3acded8d41a
James Ryan <james.ryan@cloudpractice.ca>
2021-12-03 14:36:09 -0800

    Make re-runnable and fix deleted flag

--- database/mysql/oscarhost_updates_applied/2021-12-03.migrate_relationships.sql
+++ database/mysql/oscarhost_updates_applied/2021-12-03.migrate_relationships.sql
@@ -5,7 +5,7 @@ SELECT facility_id                                 AS facilityId,
        creator                                     AS creator,
        creation_date                               AS created,
        creation_date                               AS updateDate,
-       deleted                                     AS deleted,
+       IF(deleted = 1, 1, 0)                       AS deleted,          -- Relationships table uses NULL as not deleted instead of 0
        demographic_no                              AS demographicNo,
        relation_demographic_no                     AS contactId,
        relation                                    AS role,
@@ -17,4 +17,10 @@ SELECT facility_id                                 AS facilityId,
        0                                           AS consentToContact, -- Default to no consent given as this is not captured in relationships
        1                                           AS active,
        NULL                                        AS mrp
-FROM relationships;
+FROM relationships
+WHERE (demographic_no, relation_demographic_no, relation, 1) NOT IN
+      (
+          SELECT demographic_no, contactId, role, type
+          FROM DemographicContact
+      )
+;
