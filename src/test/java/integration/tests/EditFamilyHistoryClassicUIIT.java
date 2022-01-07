3e021e69248643d5f7a5f9c7b3c64ef213d86b5d
Jordan Nicholas <jordan.nicholas@cloudpractice.ca>
2022-01-06 16:00:26 -0800

    Upgrading mockito to the version expected by spring boot

--- src/test/java/integration/tests/EditFamilyHistoryClassicUIIT.java
+++ src/test/java/integration/tests/EditFamilyHistoryClassicUIIT.java
@@ -48,8 +48,9 @@ public class EditFamilyHistoryClassicUIIT extends SeleniumTestBase
 	protected String[] getTablesToRestore()
 	{
 		return new String[]{
-				"casemgmt_cpp", "casemgmt_issue", "casemgmt_issue_notes", "casemgmt_note",
-				"casemgmt_note_ext", "eChart", "hash_audit", "log"
+			"casemgmt_cpp", "casemgmt_issue", "casemgmt_issue_notes", "casemgmt_note",
+			"casemgmt_note_ext", "eChart", "hash_audit", "log", "admission", "demographic",
+			"log_ws_rest", "measurementType", "partial_date", "property", "validations"
 		};
 	}
 
