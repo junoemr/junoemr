5738fc47795c6850879578af7b044cf1cbecd4b4
Jordan Nicholas <jordan.nicholas@cloudpractice.ca>
2022-01-14 16:17:33 -0800

    Tweaked for github

--- docker/maven/run-all-tests.sh
+++ docker/maven/run-all-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-DOCKER_USER="$(id -u):$(id -g)" USER_ID="$(id -u)" USER_GROUP_ID="$(id -g)" USER_HOME="$HOME" sudo -E docker-compose -f ${BASH_SOURCE%/*}/docker-compose.yml up --exit-code-from juno-maven
+DOCKER_USER="$(id -u):$(id -g)" USER_HOME="$HOME" sudo -E docker-compose -f ${BASH_SOURCE%/*}/docker-compose.yml up --exit-code-from juno-maven
