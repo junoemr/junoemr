6fc4f026211ce4dac2295f058773ea56c73eed7a
James Ryan <james.ryan@cloudpractice.ca>
2022-01-20 16:32:30 -0800

    Explicitly set to NULL and reformat for readbility

--- database/mysql/oscarhost_updates/2022-01-20.validate_dxresearch_icd9_entries.sql
+++ database/mysql/oscarhost_updates/2022-01-20.validate_dxresearch_icd9_entries.sql
@@ -1,7 +1,7 @@
 -- For any entry with a coding system of icd9 but the dxresearch_code isn't in the icd9 table
 -- set the coding_system to to null
 UPDATE dxresearch
-LEFT JOIN icd9 ON dxresearch_code = icd9.icd9
-SET coding_system = icd9
+LEFT JOIN icd9 ON dxresearch.dxresearch_code = icd9.icd9
+SET dxresearch.coding_system = NULL
 WHERE dxresearch.coding_system = 'icd9'
 AND icd9.icd9 IS NULL;
