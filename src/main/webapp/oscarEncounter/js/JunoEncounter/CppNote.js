3e7ee76b0f2d8d604eba6ae528a183db67e9a3b3
robert.martin <robert.martin@cloudpractice.ca>
2022-04-28 09:53:17 -0700

    fix cpp position not updating without page refresh

--- src/main/webapp/oscarEncounter/js/JunoEncounter/CppNote.js
+++ src/main/webapp/oscarEncounter/js/JunoEncounter/CppNote.js
@@ -337,11 +337,7 @@ if (!Juno.OscarEncounter.JunoEncounter.CppNote) Juno.OscarEncounter.JunoEncounte
 		// Build position dropdown
 
 		// Remove any existing options
-		var numOptions = $("position").length;
-		for(var i = 1; i <= numOptions; i++)
-		{
-			Element.remove("popt" + i)
-		}
+		junoJQuery("#position").children("option").remove();
 
 		// Add options for this list
 		var numNotes = junoJQuery("#" + summaryCode + "list li.cpp").length;
@@ -356,7 +352,7 @@ if (!Juno.OscarEncounter.JunoEncounter.CppNote) Juno.OscarEncounter.JunoEncounte
 				option.id = optId;
 				option.text = "" + opttxt;
 				option.value = j;
-				$("position").options.add(option, j);
+				junoJQuery("#position").append(option);
 				if(position === j)
 				{
 					option.selected = "selected";
