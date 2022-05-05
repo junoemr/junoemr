2339dbcd838ee4bb190ab47ea79e172154288a68
Chris Semiao <chris.semiao@cloudpractice.ca>
2022-05-05 10:23:24 -0700

    fix tabbing

--- src/main/java/oscar/oscarPrevention/reports/PreventionReport.java
+++ src/main/java/oscar/oscarPrevention/reports/PreventionReport.java
@@ -4,7 +4,7 @@
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version. 
+ * of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -58,11 +58,11 @@ public interface PreventionReport {
 
     enum PreventionReportType
     {
-        PAP,
-        MAMMOGRAM,
-        FLU,
-        CHILD_IMMUNIZATIONS,
-	FIT;
+		PAP,
+		MAMMOGRAM,
+		FLU,
+		CHILD_IMMUNIZATIONS,
+		FIT;
 
         public static PreventionReportType fromStringIgnoreCase(String enumString)
         {
