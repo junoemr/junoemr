0c179b952b3ccd66124f1d3b5717ceb24e98cea0
robert.martin <robert.martin@cloudpractice.ca>
2022-07-15 14:25:20 -0700

    don't route connectcare labs to author for specific cases, as required by conformance update

--- src/main/java/oscar/oscarLab/ca/all/parsers/AHS/v23/ConnectCareDocumentationCancelHandler.java
+++ src/main/java/oscar/oscarLab/ca/all/parsers/AHS/v23/ConnectCareDocumentationCancelHandler.java
@@ -24,19 +24,13 @@ package oscar.oscarLab.ca.all.parsers.AHS.v23;
 
 import ca.uhn.hl7v2.HL7Exception;
 import ca.uhn.hl7v2.model.Message;
-import ca.uhn.hl7v2.model.v23.datatype.CX;
 import ca.uhn.hl7v2.model.v23.message.MDM_T11;
 import ca.uhn.hl7v2.model.v23.segment.MSH;
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang3.tuple.Pair;
 import org.oscarehr.common.model.Hl7TextInfo;
 import oscar.oscarLab.ca.all.parsers.AHS.ConnectCareLabType;
-import oscar.oscarLab.ca.all.parsers.messageTypes.MDM_T11MessageHandler;
+import oscar.oscarLab.ca.all.parsers.AHS.MDM_T11ConnectCareHandler;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class ConnectCareDocumentationCancelHandler extends MDM_T11MessageHandler
+public class ConnectCareDocumentationCancelHandler extends MDM_T11ConnectCareHandler
 {
 	public ConnectCareDocumentationCancelHandler(Message msg) throws HL7Exception
 	{
@@ -87,70 +81,6 @@ public class ConnectCareDocumentationCancelHandler extends MDM_T11MessageHandler
 	public void init(String hl7Body) throws HL7Exception
 	{
 	}
-	/* ================================= PID ============================== */
-
-	/**
-	 * Connect Care labs send more than just health card number, they can also send, EPI, ABH, NWT, BKR
-	 * @param appendNamespace - if true append namespace to end of identifier
-	 * @return - list of paris patient identification &lt;id type , id + assigning authority >
-	 */
-	public ArrayList<Pair<String, String>> getPatientIdentificationList(boolean appendNamespace)
-	{
-		ArrayList<Pair<String, String>> ids = new ArrayList<Pair<String, String>>();
-		if (message instanceof MDM_T11)
-		{
-			MDM_T11 msg = (MDM_T11) message;
-			for (int i =0; i < msg.getPID().getPatientIDInternalIDReps(); i ++)
-			{
-				CX id = msg.getPID().getPatientIDInternalID(i);
-				if (id.getAssigningAuthority().getNamespaceID().getValue() != null && id.getID().getValue() != null &&
-						id.getIdentifierTypeCode().getValue() != null)
-				{
-					String idString = id.getID().getValue();
-					if (appendNamespace)
-					{
-						idString += " " + id.getAssigningAuthority().getNamespaceID().getValue();
-					}
-					ids.add(Pair.of(id.getIdentifierTypeCode().getValue(), idString));
-				}
-			}
-			return ids;
-		}
-		else
-		{
-			return new ArrayList<Pair<String, String>>();
-		}
-	}
-
-	/**
-	 * pull patient health number from the patient identifier list
-	 * @return - the patients health number
-	 */
-	@Override
-	public String getHealthNum()
-	{
-		ArrayList<Pair<String, String>> patientIds = getPatientIdentificationList(false);
-		for (Pair<String, String> id : patientIds)
-		{
-			if (id.getLeft().equalsIgnoreCase("PHN") || id.getLeft().equalsIgnoreCase("ULI"))
-			{
-				return id.getRight();
-			}
-		}
-		return null;
-	}
-
-	/* ================================= OBR ============================== */
-
-	/**
-	 *  Even though message has no OBR, return 1 for display purposes
-	 */
-	@Override
-	public int getOBRCount()
-	{
-		return 1;
-	}
-
 
 	/**
 	 * always return cancelled as the status
@@ -160,104 +90,6 @@ public class ConnectCareDocumentationCancelHandler extends MDM_T11MessageHandler
 	public Hl7TextInfo.REPORT_STATUS getJunoOrderStatus()
 	{
 		return Hl7TextInfo.REPORT_STATUS.X;
-	}
-
-	@Override
-	public String getCCDocs()
-	{
-		try
-		{
-			return String.join(", ", getCCDocNames());
-		}
-		catch(HL7Exception e)
-		{
-			throw new RuntimeException(e);
-		}
-	}
-
-	protected List<String> getCCDocNames() throws HL7Exception
-	{
-		List<String> docNames = new ArrayList<>();
-		int txa_23Count = getFieldReps("/.TXA", 23);
-		for(int k = 0; k < txa_23Count; k++)
-		{
-			String docName = getResultCopiesTo(0, k);
-			if(StringUtils.isNotBlank(docName))
-			{
-				docNames.add(docName);
-			}
-		}
-
-		// add pv1 provider to cc docs if not marked confidential
-		if(!isReportBlocked())
-		{
-			int pd1_4Count = getFieldReps("/.PD1", 4);
-			for(int k = 0; k < pd1_4Count; k++)
-			{
-				String docName = getFullDocName("/.PD1", 4, k);
-				if(StringUtils.isNotBlank(docName))
-				{
-					docNames.add(docName);
-				}
-			}
-		}
-		return docNames;
-	}
-
-	@Override
-	public List<String> getDocNums()
-	{
-		List<String> docIds = new ArrayList<>();
-		try
-		{
-			String providerId = getOrderingProviderNo(0, 0);
-			docIds.add(providerId);
-
-			int txa_23Count = getFieldReps("/.TXA", 23);
-			for(int k = 0; k < txa_23Count; k++)
-			{
-				String docId = getResultCopiesToProviderNo(0, k);
-				if(StringUtils.isNotBlank(docId))
-				{
-					docIds.add(docId);
-				}
-			}
-
-			// add pv1 provider to cc docs if not marked confidential
-			if(!isReportBlocked())
-			{
-				int pd1_4Count = getFieldReps("/.PD1", 4);
-				for(int k = 0; k < pd1_4Count; k++)
-				{
-					String docId = get("/.PD1-4(" + k + ")-1");
-					if(StringUtils.isNotBlank(docId))
-					{
-						docIds.add(docId);
-					}
-				}
-			}
-		}
-		catch(Exception e)
-		{
-			logger.error("Could not return doctor nums", e);
-		}
-		return docIds;
-	}
-
-	/**
-	 * indicates blocked or sensitive data within the report
-	 * @return true if report flagged as sensitive/confidential
-	 */
-	@Override
-	public boolean isReportBlocked()
-	{
-		switch(getString(get("/.TXA-18")))
-		{
-			case "RE":              // Restricted
-			case "VR": return true; // Very restricted
-			case "UC":              // Usual control
-			default: return false;
-		}
 	}
 
 	/* ========================== OBX ========================== */
