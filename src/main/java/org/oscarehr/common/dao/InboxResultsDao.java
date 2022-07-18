251f9c80db8f3128745dd406bb9ac07a054cef6b
robert.martin <robert.martin@cloudpractice.ca>
2022-07-18 16:16:26 -0700

    updated inbox hrm filter with readability + fixes from review

--- src/main/java/org/oscarehr/common/dao/InboxResultsDao.java
+++ src/main/java/org/oscarehr/common/dao/InboxResultsDao.java
@@ -153,7 +153,7 @@ public class InboxResultsDao
 			String filterSql = "";
 			String proLrSql = "";
 			String labSql = "";
-			String hrmFilterSql = "";
+			String hrmJoinSql = "";
 			
 			if (labType != null && !"".equals(labType))
 			{
@@ -181,19 +181,19 @@ public class InboxResultsDao
 			if ("-1".equals(providerNo) || "".equals(providerNo))
 			{
 				// any provider
+				hrmJoinSql += "AND false ";
 			}
 			else if ("0".equals(providerNo))
 			{
 				// unclaimed
-				filterSql += "AND (proLR.provider_no = '0') ";
+				filterSql += "AND (proLR.provider_no = '0') AND (hrmProv.providerNo IS NULL) ";
 				proLrSql += "AND (proLR_filter.provider_no = '0') ";
-				hrmFilterSql += "LEFT JOIN HRMDocumentToProvider hrmProv ON (hrm.id = hrmProv.hrmDocumentId AND proLR_filter.provider_no = '0') ";
 			}
 			else
 			{
 				filterSql += "AND (proLR.provider_no = :provider_no) ";
 				proLrSql += "AND (proLR_filter.provider_no = :provider_no) ";
-				hrmFilterSql += "LEFT JOIN HRMDocumentToProvider hrmProv ON (hrm.id = hrmProv.hrmDocumentId AND proLR_filter.provider_no = :provider_no) ";
+				hrmJoinSql += "AND (hrmProv.providerNo = :provider_no) ";
 				qp_provider_no = true;
 			}
 			
@@ -448,9 +448,9 @@ public class InboxResultsDao
 				+ labSql
 
 				+ "LEFT JOIN HRMDocument hrm ON ( proLR.lab_type ='HRM' AND proLR.lab_no = hrm.id ) "
+				+ "LEFT JOIN HRMDocumentToProvider hrmProv ON (hrm.id = hrmProv.hrmDocumentId " + hrmJoinSql + ") "
 				+ "LEFT JOIN HRMDocumentToDemographic hrmDemo ON hrm.id = hrmDemo.hrmDocumentId "
 				+ "LEFT JOIN demographic d3 ON hrmDemo.demographicNo = d3.demographic_no "
-				+ hrmFilterSql
 
 				+ "WHERE proLR.lab_type IN ('DOC', 'HL7', 'HRM') "
 				+ "AND proLR_filter.id IS NULL "
