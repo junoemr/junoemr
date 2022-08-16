5dae2c56d989e05c86cd3525ee9286f4b01a7512
Evan Vassiliadis <evan.vassiliadis@cloudpractice.ca>
2022-08-16 16:23:43 -0700

    Fixed typo in alter table statement for column name

--- database/mysql/oscarhost_updates/2022-08-15.service_specialists_not_null.sql
+++ database/mysql/oscarhost_updates/2022-08-15.service_specialists_not_null.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 DELETE FROM serviceSpecialists WHERE serviceId IS NULL OR specId IS NULL;
 
-ALTER TABLE serviceSpecialists MODIFY serviceID int(10) NOT NULL;
+ALTER TABLE serviceSpecialists MODIFY serviceId int(10) NOT NULL;
 ALTER TABLE serviceSpecialists MODIFY specId int(10) NOT NULL;
 
 COMMIT;
